ee80be1a406fbdddc85a006f11847231c1a610fc
Erik Pohl <iotasubscript@gmail.com>
2018-10-21 10:10:39 -0400

    added another condition
    check for missing keys in test data

--- python/JSONQuery/JSONQuery.py
+++ python/JSONQuery/JSONQuery.py
@@ -6,10 +6,10 @@ def json_parse(test_data, format_data, usedpath='', mismatches =[], debugmode = 
     and a format_data json variable
     and a debugmode variable
     usedpath and mismatches are for internal use only
-    
+
     test_data is the json you want to compare against a particular format
     format_data is the json format with regex expressions for values
-    
+
     return value are any paths to json keys which have a mismatch
     between the test_data value for those keys and the corresponding format_data'''
     if debugmode == 1:
@@ -18,6 +18,13 @@ def json_parse(test_data, format_data, usedpath='', mismatches =[], debugmode = 
     logging.info("parsing format data"+ str(format_data))
     for i in format_data.keys():
         logging.info("evaluating key:"+ i)
+        try:
+            z = test_data[i]
+        except:
+            logging.info("key not found")
+            logging.info("usedpath" + usedpath + '/' + i)
+            mismatches.append(usedpath + '/' + i)
+            continue
         logging.info("format value for the key" +str(type(format_data[i]))+ str(len(format_data[i]))+ str(format_data[i]))
         logging.info("test value for the key"+ str(type(test_data[i]))+ str(len(test_data[i]))+ str(test_data[i]))
         if isinstance(format_data[i], dict) and len(format_data[i]) > 1:
@@ -35,13 +42,9 @@ def json_parse(test_data, format_data, usedpath='', mismatches =[], debugmode = 
     return mismatches
 
 test_json_str =         '{"hello":"1", "zarg": {"h1":"one", "h2":"2"}}'
-json_query_format_str = '{"hello":"2", "zarg": {"h1":"one", "h2":"3"}}'
+json_query_format_str = '{"hello":"2", "zarg": {"h1":"one", "h2":"3"}, "blammo": "1"}'
 test_json = json.loads(test_json_str)
 json_query_format = json.loads(json_query_format_str)
 print("starting test_json", test_json)
 print("starting json_query_format", json_query_format)
 print("mismatches", json_parse(test_json,json_query_format))
-
-
-
-
