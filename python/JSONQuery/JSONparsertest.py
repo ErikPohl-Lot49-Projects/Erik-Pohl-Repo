6612a4579e63be2af027005f36f5a70bc0481b3a
Erik Pohl <iotasubscript@gmail.com>
2018-10-22 02:39:57 -0400

    add matches reported cases
    add cases where matches are reported, not mismatches

--- python/JSONQuery/JSONparsertest.py
+++ python/JSONQuery/JSONparsertest.py
@@ -9,23 +9,31 @@ import unittest as ut
 
 class JSONParsertest(ut.TestCase):
     @pm.parameterized.expand([
-        ("Basic match", {'hello':'1'},{'hello':'1'}, []),
-        ("Basic Wildcard match", {'hello': '1'}, {'hello': '.'}, []),
-        ("Basic mismatch", {'hello': '1'}, {'hello': 'x'}, ['/hello']),
+        ("Basic match", {'hello':'1'},{'hello':'1'}, 0, []),
+        ("Basic Wildcard match", {'hello': '1'}, {'hello': '.'}, 0, []),
+        ("Basic mismatch", {'hello': '1'}, {'hello': 'x'}, 0,['/hello']),
         ("Nested match", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '1'}},
-         {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}, []),
+         {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}, 0,[]),
         ("Nested mismatch level 1", {'hello': '2', 'zap': {'h1': 'one', 'h2': 'two', 'single': '1'}},
-         {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}, ['/hello']),
+         {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}, 0,['/hello']),
         ("Nested mismatch level 1 and 2", {'hello': '2', 'zap': {'h1': 'one', 'h2': 'two', 'single': '1'}},
-         {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '2'}}, ['/hello', '/zap/single']),
+         {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '2'}}, 0,['/hello', '/zap/single']),
         ("Unexpected format key", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '1'}},
-         {'hello': '.', 'blammo' : 'blam', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}, ['/blammo'] ),
+         {'hello': '.', 'blammo' : 'blam', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}, 0,['/blammo'] ),
         ("Nested match with fewer keys in format", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
-         {'zap': {'h1': 'one'}}, []),
+         {'zap': {'h1': 'one'}}, 0,[]),
         ("Nested mismatch with fewer keys in format", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
-         {'zap': {'h1': 'onx'}}, ['/zap/h1']),
+         {'zap': {'h1': 'onx'}}, 0,['/zap/h1']),
         ("Nested mismatches with fewer keys in format", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '4'}},
-         {'zap': {'h1': 'onx', 'single': '5'}}, [['/zap/h1', '/zap/single']]),
+         {'zap': {'h1': 'onx', 'single': '5'}}, 0,['/zap/h1', '/zap/single']),
+        ("report matches all ands satisfied with nesting",{'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
+         {"zap": {"h1": ".*"}, "hello": ".*"},1,[('/zap/h1', 'one'), ('/hello', '1')]),
+        ("report matches one and not satisfied with nesting",
+         {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
+         {"zap": {"h1": ".*"}, "hello": "3"}, 1, []),
+        ("report matches all ands satisfied without nesting of format",
+         {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
+         {"hello": "."}, 1, [('/hello', '1')]),
     ])
-    def testJSONparser(self, testname, testinp, testinp2, expected):
-        self.assertEqual(JP.json_format_compare(testinp, testinp2, '', []), expected)
+    def testJSONparser(self, testname, testinp, testinp2, matchtype, expected):
+        self.assertEqual(expected,JP.json_format_compare(testinp, testinp2, matchmode=matchtype))
