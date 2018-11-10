342dc7d36cb0cf50603b793166d21763860408a5
Erik Pohl <iotasubscript@gmail.com>
2018-11-10 08:47:29 -0500

    adds an or mode
    needs lots of refactoring

--- python/JSONQuery/JSONQuery.py
+++ python/JSONQuery/JSONQuery.py
@@ -1,6 +1,7 @@
 import json, re, logging, sys
 
-#TODO I will possibly make a marker for an AND match attempt and another marker for an OR match attempt
+#TODO refactor
+
 def json_format_compare(test_data, format_data, usedpath='', results =None,debugmode = 0, matchmode =0):
     '''json_format_compare
     accepts as input a test_data json variable
@@ -14,9 +15,8 @@ def json_format_compare(test_data, format_data, usedpath='', results =None,debug
     results are either full matches in the test_data to the format_data if match mode is set to find matches
     or a list of mismatches if match mode is set to find mismatches
     '''
-    
+
     if results == None:
-        print("clearing")
         results = []
     if debugmode:
         logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -58,7 +58,11 @@ def json_format_compare(test_data, format_data, usedpath='', results =None,debug
                     logging.info("adding current json path to results: "+ current_json_path)
                     results.append(current_json_path)
                 else: ## partial matches are not okay
-                    return []
+                    if matchmode == 2: # or mode
+                        # this is a mismatch, but don't short circuit and exit because we're in or mode
+                        pass
+                    else: # and mode
+                        return []
     return results
 
 test_json_str =         '{"hello": "1", "zap": {"h1": "one", "h2": "two", "single": "."}}'
@@ -67,6 +71,6 @@ test_json = json.loads(test_json_str)
 json_query_format = json.loads(json_query_format_str)
 print("starting test_json", test_json)
 print("starting json_query_format", json_query_format)
-print("mismatches", json_format_compare(test_json, json_query_format, debugmode=1))
+print("mismatches", json_format_compare(test_json, json_query_format, debugmode=0))
 
 print("matches", json_format_compare(test_json, json_query_format, matchmode=1, debugmode=1))
