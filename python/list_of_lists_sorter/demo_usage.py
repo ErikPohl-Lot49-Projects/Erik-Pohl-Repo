9bfaebdd63dee5fcb967c961c2ccb6ede19e6ea0
Erik Pohl <iotasubscript@gmail.com>
2018-11-14 16:07:25 -0500

    refactored demo usage code
    refactored demo usage code

--- python/list_of_lists_sorter/demo_usage.py
+++ python/list_of_lists_sorter/demo_usage.py
@@ -10,37 +10,31 @@ output_list_with_header = [
     [1, 2, 3, 2, 6, 2, '1-1-10'],
     [1, 2, 3, 1, 7, 1, '1-1-18']
 ]
+output_list_without_header = [
+    [1, 2, 3, 7, 1, 7, '1-1-15'],
+    [1, 2, 3, 7, 2, 6, '1-1-14'],
+    [1, 2, 3, 5, 3, 5, '1-1-13'],
+    [1, 2, 3, 4, 4, 4, '1-1-12'],
+    [1, 2, 3, 3, 5, 3, '1-1-11'],
+    [1, 2, 3, 2, 6, 2, '1-1-10'],
+    [1, 2, 3, 1, 7, 1, '1-1-18']
+]
+
 sorter = list_of_list_sorter(output_list_with_header)
 sorter.has_header = True
 sorter.add_sort_field_by_header_field_name('six', 'datestringdel-')
 print(sorter.sort())
 
-output_list_without_header = [
-    [1, 2, 3, 7, 1, 7, '1-1-15'],
-    [1, 2, 3, 7, 2, 6, '1-1-14'],
-    [1, 2, 3, 5, 3, 5, '1-1-13'],
-    [1, 2, 3, 4, 4, 4, '1-1-12'],
-    [1, 2, 3, 3, 5, 3, '1-1-11'],
-    [1, 2, 3, 2, 6, 2, '1-1-10'],
-    [1, 2, 3, 1, 7, 1, '1-1-18']
-]
-sorter = list_of_list_sorter(output_list_without_header)
+sorter.clear_sort_fields()
+sorter.add_sort_field_by_position(6, 'datestringdel-')
 sorter.has_header = False
 sorter.reverse_sort = True
-sorter.add_sort_field_by_position(6, 'datestringdel-')
+sorter.list_of_lists =output_list_without_header
 print(sorter.sort())
 
-output_list_without_header = [
-    [1, 2, 3, 7, 1, 7, '1-1-15'],
-    [1, 2, 3, 7, 2, 6, '1-1-14'],
-    [1, 2, 3, 5, 3, 5, '1-1-13'],
-    [1, 2, 3, 4, 4, 4, '1-1-12'],
-    [1, 2, 3, 3, 5, 3, '1-1-11'],
-    [1, 2, 3, 2, 6, 2, '1-1-10'],
-    [1, 2, 3, 1, 7, 1, '1-1-18']
-]
-sorter = list_of_list_sorter(output_list_without_header)
+sorter.clear_sort_fields()
+sorter.add_multiple_fields_by_position([(6, 'datestringdel-'), (4, 'int')])
 sorter.has_header = False
 sorter.reverse_sort = False
-sorter.add_multiple_fields_by_position([(6, 'datestringdel-'), (4, 'int')])
+sorter.list_of_lists =output_list_without_header
 print(sorter.sort())
