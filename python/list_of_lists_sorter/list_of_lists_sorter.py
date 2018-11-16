6886058b4bf1325039cb99a1816ded0cebd765ad
Erik Pohl <iotasubscript@gmail.com>
2018-11-15 22:24:55 -0500

    added docstrings
    added docstrings

--- python/list_of_lists_sorter/list_of_lists_sorter.py
+++ python/list_of_lists_sorter/list_of_lists_sorter.py
@@ -3,6 +3,11 @@ from datetime import date
 
 
 class list_of_list_sorter:
+    '''
+    This class allows you to flexibly define and execute sorts on lists of lists
+    with and without headers
+    with and without reverse sort
+    '''
     sort_field = namedtuple('sort_field', 'position field_type')
 
     STRINGDATEPREFIX = 'datestringdelimiter'
@@ -14,13 +19,22 @@ class list_of_list_sorter:
         self.has_header = False
 
     def add_sort_field_by_position(self, column_position, sort_field_type="string"):
+        '''
+        add a sort field to the sort field criteria by position in the list, sort field type default is string
+        '''
         self.sort_fields.append(self.sort_field(column_position, sort_field_type))
 
     def add_sort_field_by_header_field_name(self, field_name, sort_field_type="string"):
+        '''
+        add a sort field to the sort field criteria by header field name in the list, sort field type default is string
+        '''
         if self.has_header:
             self.sort_fields.append(self.sort_field(self.list_of_lists[0].index(field_name), sort_field_type))
 
     def add_multiple_fields_by_position(self, position_type_list):
+        '''
+        add a multiple sort fields to the sort field criteria by position in the list, sort field type default is string
+        '''
         for sort_field_add in position_type_list:
             if type(sort_field_add) is not tuple:
                 sort_field_add = (sort_field_add, "string")
@@ -37,6 +51,9 @@ class list_of_list_sorter:
          for sort_field_add in position_type_list]
 
     def add_multiple_fields_by_header_field(self, position_type_list):
+        '''
+        add a multiple sort fields to the sort field criteria by header field name in the list, sort field type default is string
+        '''
         for sort_field_add in position_type_list:
             if type(sort_field_add) is not tuple:
                 sort_field_add = (sort_field_add, "string")
@@ -53,18 +70,30 @@ class list_of_list_sorter:
          for sort_field_add in position_type_list]
 
     def clear_sort_fields(self):
+        '''
+        clear the sort fields criteria list
+        '''
         self.sort_fields = []
 
     def field_type_convert(self, raw, field_type):
+        '''
+        convert the field data from string to a date if applicable
+        '''
         datetuple = namedtuple('datetuple', 'month day year')
         if field_type.startswith(self.STRINGDATEPREFIX):
             return datetuple(*raw.split(field_type[-1]))
         return raw
 
     def sort_choice(self, x, its):
+        '''
+        define the sort type based on the sort field list of criteria
+        '''
         return [self.field_type_convert(x[y.position], y.field_type) for y in its]
 
     def sort(self):
+        '''
+        execute the sort based on all of the criteria and setups in the instantiation
+        '''
         header_offset = int(self.has_header)
         if self.has_header:
             copyheader = self.list_of_lists[0]
