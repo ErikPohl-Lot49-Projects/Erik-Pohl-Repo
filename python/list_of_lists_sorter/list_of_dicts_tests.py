c86d30e79c503d4cc226593025f5eb5a12f1f26b
Erik Pohl <iotasubscript@gmail.com>
2018-11-17 10:54:46 -0500

    Tests for list_of_dicts_sorter
    Since lists of dicts don't have headers like lists of lists do, this has a lot fewer cases and complexity

---
+++ python/list_of_lists_sorter/list_of_dicts_tests.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+from list_of_dicts_sorter import list_of_dicts_sorter
+
+
+class ListOfDictsTests(TestCase):
+
+    def testCaseBasicInitialization(self):
+        output_dict = [
+            [{'one': 1, 'two': 5}],
+            [{'one': 1, 'two': 4}]
+        ]
+        lodsorter = list_of_dicts_sorter(output_dict)
+        self.assertEqual(lodsorter.list_of_dicts, output_dict)  # initialized value
+        self.assertEqual(lodsorter.reverse_sort, False)  # default value
+
+    def testCaseTestDateSort(self):
+        output_list_of_dicts = [
+            {'one': 1, 'two': '1-1-15'},
+            {'one': 1, 'two': '1-1-14'}
+        ]
+        lodsorter = list_of_dicts_sorter(output_list_of_dicts)
+        lodsorter.add_sort_field_by_key_name('two','datestringdelimiter-')
+        sorted_output = lodsorter.sort()
+        self.assertEqual(sorted_output, [
+            {'one': 1, 'two': '1-1-14'},
+            {'one': 1, 'two': '1-1-15'}
+        ])
+        lodsorter.reverse_sort = True
+        sorted_output = lodsorter.sort()
+        self.assertEqual(sorted_output, [
+            {'one': 1, 'two': '1-1-15'},
+            {'one': 1, 'two': '1-1-14'}
+        ])
+
+    def testCaseNumberOnlySort(self):
+        output_list_of_dicts = [
+            {'one': 1, 'two': '5'},
+            {'one': 1, 'two': '4'}
+        ]
+        lodsorter = list_of_dicts_sorter(output_list_of_dicts)
+        lodsorter.add_sort_field_by_key_name('two')
+        sorted_output = lodsorter.sort()
+        self.assertEqual(sorted_output, [
+            {'one': 1, 'two': '4'},
+            {'one': 1, 'two': '5'}
+        ])
+        lodsorter.reverse_sort = True
+        sorted_output = lodsorter.sort()
+        self.assertEqual(sorted_output, [
+            {'one': 1, 'two': '5'},
+            {'one': 1, 'two': '4'}
+        ])
+
