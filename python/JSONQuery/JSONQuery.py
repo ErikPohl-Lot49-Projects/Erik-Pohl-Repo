892afe1a86983dc7eaf1d16f8ab1f2d669fddd27
Erik Pohl <iotasubscript@gmail.com>
2018-11-19 17:31:24 -0500

    altered return values
    likely to do so again soon

--- python/JSONQuery/JSONQuery.py
+++ python/JSONQuery/JSONQuery.py
@@ -2,9 +2,8 @@ import json, re, logging, sys
 from collections import namedtuple
 
 #TODO make it a class with a compare method
-#TODO should a mismatch which is actually mismatched return an 'actual_finding_value' of the input json
 json_query_finding = namedtuple('json_query_finding', 'current_json_path, actual_finding_value')
-json_query_final_results = namedtuple('json_query_results', 'json_query_finding, overall_result')
+json_query_final_results = namedtuple('json_query_results', 'json_query_finding, all_criteria_met')
 JSON_KEY_MISSING = 'JSON_key_not_found'
 JSON_VALUE_MISMATCH = 'JSON_value_mismatch'
 MATCHMODES = {'AND': 1, 'OR': 2, 'MISMATCH':0}
@@ -22,8 +21,9 @@ def compare_json_to_query_clause(JSON_to_query, JSON_query_clause, *,
     :param JSON_query_results: This stores results of the comparison throughout the query
     :return: This returns findings based on the JSON_to_query, the JSON_query_clause, and the match_mode
     '''
+    all_criteria_met = True
     if JSON_query_results == None:
-        JSON_query_results = []
+        JSON_query_results  = []
     if debug_mode:
         logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     for format_key in JSON_query_clause.keys():
@@ -39,21 +39,24 @@ def compare_json_to_query_clause(JSON_to_query, JSON_query_clause, *,
         JSON_query_key_value = JSON_query_clause[format_key]
         ## if the format value which is being compared with the test value is itself a clause, recurse
         if isinstance(JSON_query_key_value, dict):
-            compare_json_to_query_clause(JSON_to_query_key_value, JSON_query_key_value,
+            x = compare_json_to_query_clause(JSON_to_query_key_value, JSON_query_key_value,
                                          current_JSON_path=current_json_path,
                                          JSON_query_results=JSON_query_results,
                                          match_mode=match_mode, debug_mode=debug_mode)
+            all_criteria_met = x.all_criteria_met
         else:
             if re.match(JSON_query_key_value, JSON_to_query_key_value):
                 if match_mode:
                     JSON_query_results.append(json_query_finding(current_json_path, JSON_to_query_key_value))
+                else:
+                    all_criteria_met = False
             else:
                 if not match_mode: #mismach is being tracked
                     JSON_query_results.append(json_query_finding(current_json_path, JSON_VALUE_MISMATCH))
                 else:  ## partial matches are not okay for ANDs but are okay for ORs
                     if match_mode == MATCHMODES["AND"]:
-                        return json_query_final_results([],None)
+                        return json_query_final_results([],False)
+                    else:
+                        all_criteria_met = False
                         # otherwise, this is a mismatch, but don't short circuit and exit because we're in or mode
-    if (match_mode) and (JSON_query_results):
-        return json_query_final_results(JSON_query_results, JSON_to_query)
-    return json_query_final_results(JSON_query_results, None)
+    return json_query_final_results(JSON_query_results, all_criteria_met)
