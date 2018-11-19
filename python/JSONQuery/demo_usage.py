ca03935f60037edab4bf63c933f5a2eb8c022040
Erik Pohl <iotasubscript@gmail.com>
2018-11-19 17:34:27 -0500

    updated
    updated

--- python/JSONQuery/demo_usage.py
+++ python/JSONQuery/demo_usage.py
@@ -1,5 +1,5 @@
 from json import loads
-from JSONparser import json_format_compare
+from JSONparser import compare_json_to_query_clause
 
 test_json_str =         '{"hello": "1", "zap": {"h1": "one", "h2": "two", "single": "."}}'
 json_query_format_str = '{"zap": {"h1": ".*"}, "hello": ".*"}'
@@ -7,9 +7,9 @@ test_json = loads(test_json_str)
 json_query_format = loads(json_query_format_str)
 print("starting test_json", test_json)
 print("starting json_query_format", json_query_format)
-print("mismatches", json_format_compare(test_json, json_query_format, debugmode=0))
-print("matches", json_format_compare(test_json, json_query_format, matchmode=1, debugmode=1))
+print("mismatches", compare_json_to_query_clause(test_json, json_query_format, debug_mode=0))
+print("matches", compare_json_to_query_clause(test_json, json_query_format, match_mode=1, debug_mode=1))
 test_json = {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}
 json_query_format= {"zap": {"h1": ".*"}, "hello": ".*"}
-matches = json_format_compare(test_json, json_query_format, matchmode=2, debugmode=1)
+matches = compare_json_to_query_clause(test_json, json_query_format, match_mode=2, debug_mode=1)
 print("matches", matches)
