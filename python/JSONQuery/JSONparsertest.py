47b0582ccf22b2022c33f59132074b51e3c7a497
Erik Pohl <iotasubscript@gmail.com>
2018-11-19 17:33:32 -0500

    updated test cases
    updated test cases

--- python/JSONQuery/JSONparsertest.py
+++ python/JSONQuery/JSONparsertest.py
@@ -15,40 +15,40 @@ class JSONParsertest(ut.TestCase):
         ("Basic Wildcard match", {'hello': '1'}, {'hello': '.'}, MATCHMODES["MISMATCH"], []),
 
         ("Basic mismatch", {'hello': '1'}, {'hello': 'x'}, MATCHMODES["MISMATCH"],
-         [json_query_finding(current_json_path='/hello', queried_json_clause=JSON_VALUE_MISMATCH)]),
+         [json_query_finding(current_json_path='/hello', actual_finding_value=JSON_VALUE_MISMATCH)]),
 
         ("Nested match", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '1'}},
          {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}, MATCHMODES["MISMATCH"],[]),
 
         ("Nested mismatch level 1", {'hello': '2', 'zap': {'h1': 'one', 'h2': 'two', 'single': '1'}},
          {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}, MATCHMODES["MISMATCH"],
-         [json_query_finding(current_json_path='/hello', queried_json_clause=JSON_VALUE_MISMATCH)]),
+         [json_query_finding(current_json_path='/hello', actual_finding_value=JSON_VALUE_MISMATCH)]),
 
         ("Nested mismatch level 1 and 2", {'hello': '2', 'zap': {'h1': 'one', 'h2': 'two', 'single': '1'}},
          {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '2'}}, MATCHMODES["MISMATCH"],
-         [json_query_finding(current_json_path='/hello', queried_json_clause=JSON_VALUE_MISMATCH),
-          json_query_finding(current_json_path='/zap/single', queried_json_clause=JSON_VALUE_MISMATCH)]),
+         [json_query_finding(current_json_path='/hello', actual_finding_value=JSON_VALUE_MISMATCH),
+          json_query_finding(current_json_path='/zap/single', actual_finding_value=JSON_VALUE_MISMATCH)]),
 
         ("Unexpected format key", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '1'}},
          {'hello': '.', 'blammo' : 'blam', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}}, MATCHMODES["MISMATCH"],
-         [json_query_finding(current_json_path='/blammo', queried_json_clause=JSON_KEY_MISSING)]),
+         [json_query_finding(current_json_path='/blammo', actual_finding_value=JSON_KEY_MISSING)]),
 
         ("Nested match with fewer keys in format", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
          {'zap': {'h1': 'one'}}, MATCHMODES["MISMATCH"],[]),
 
         ("Nested mismatch with fewer keys in format", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
          {'zap': {'h1': 'onx'}}, MATCHMODES["MISMATCH"],
-         [json_query_finding(current_json_path='/zap/h1', queried_json_clause=JSON_VALUE_MISMATCH)]),
+         [json_query_finding(current_json_path='/zap/h1', actual_finding_value=JSON_VALUE_MISMATCH)]),
 
         ("Nested mismatches with fewer keys in format", {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '4'}},
          {'zap': {'h1': 'onx', 'single': '5'}}, MATCHMODES["MISMATCH"],
-         [json_query_finding(current_json_path='/zap/h1', queried_json_clause=JSON_VALUE_MISMATCH),
-          json_query_finding(current_json_path='/zap/single', queried_json_clause=JSON_VALUE_MISMATCH)]),
+         [json_query_finding(current_json_path='/zap/h1', actual_finding_value=JSON_VALUE_MISMATCH),
+          json_query_finding(current_json_path='/zap/single', actual_finding_value=JSON_VALUE_MISMATCH)]),
 
         ("report matches all ands satisfied with nesting",{'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
          {"zap": {"h1": ".*"}, "hello": ".*"},MATCHMODES["AND"],
-         [json_query_finding(current_json_path='/zap/h1', queried_json_clause='one'),
-          json_query_finding(current_json_path='/hello', queried_json_clause='1')]),
+         [json_query_finding(current_json_path='/zap/h1', actual_finding_value='one'),
+          json_query_finding(current_json_path='/hello', actual_finding_value='1')]),
 
         ("report matches one and not satisfied with nesting",
          {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
@@ -57,14 +57,15 @@ class JSONParsertest(ut.TestCase):
         ("report matches all ands satisfied without nesting of format",
          {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
          {"hello": "."}, MATCHMODES["AND"],
-         [json_query_finding(current_json_path='/hello', queried_json_clause='1')]),
+         [json_query_finding(current_json_path='/hello', actual_finding_value='1')]),
 
         ("report or matches",
          {'hello': '1', 'zap': {'h1': 'one', 'h2': 'two', 'single': '.'}},
          {"hello": ".", 'zap': {'h1': 'ox'}}, MATCHMODES["OR"],
-         [json_query_finding(current_json_path='/hello', queried_json_clause='1')]),
+         [json_query_finding(current_json_path='/hello', actual_finding_value='1')]),
     ])
     def testJSONparser(self, testname, testinp, testinp2, matchtype, expected):
         actual = JP.compare_json_to_query_clause(testinp, testinp2, match_mode=matchtype, debug_mode=True)
-        print(actual.overall_result)
+        #if testname == 'Nested mismatches with fewer keys in format':
+        #    print(actual.json_query_finding)
         self.assertEqual(expected, actual.json_query_finding)
