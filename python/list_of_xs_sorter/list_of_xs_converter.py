7439e8d96388cbf977f8fda9d5ba9d475e989c8e
Erik Pohl <iotasubscript@gmail.com>
2018-11-24 08:59:00 -0500

    new function, not cleaned up
    this converts one list of x type to another list of x type

---
+++ python/list_of_xs_sorter/list_of_xs_converter.py
@@ -0,0 +1,22 @@
+def list_of_xs_converter(*,list_of_xs=None,to_list_of =None, output_as_string_delimiter = ' '):
+    if type(list_of_xs[0]) == dict:
+        decision_dict = {list: [[[key for key in list_of_xs[0]]] +
+                                [[dictval for dictval in dict.values()] for dict in
+                                 list_of_xs]][0],
+                         dict: list_of_xs,
+                         str: [output_as_string_delimiter.join(x) for x in [[[str(key) for key in list_of_xs[0]]] +
+                               [[str(dictval) for dictval in dict.values()] for dict in
+                                 list_of_xs]][0]]
+                         }
+        return decision_dict[to_list_of]
+    if type(list_of_xs[0]) == list:
+        decision_dict = {dict:
+                             [{list_of_xs[0][lcount]:litem for lcount, litem in enumerate(list)} for list in list_of_xs[1:]],
+                         list: list_of_xs,
+                         str: list_of_xs}
+        return decision_dict[to_list_of]
+    if type(list_of_xs[0]) == str:
+        decision_dict = {dict: list_of_xs,
+                         list: list_of_xs,
+                         str: list_of_xs}
+        return decision_dict[to_list_of]
