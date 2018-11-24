4fa39ba123be5dffcc5993b3efd326d95377d0f1
Erik Pohl <iotasubscript@gmail.com>
2018-11-24 17:26:50 -0500

    added docsys
    added docsys

--- python/list_of_xs_sorter/list_of_xs_converter.py
+++ python/list_of_xs_sorter/list_of_xs_converter.py
@@ -2,7 +2,19 @@
 # I wanted to prove to myself I could do this in list comprehensions and dict comprehensions
 # AND I DID BWAH HA HA HA HA
 # you want readable code?  I could do that, too.  Easier.
-def list_of_xs_converter(*, list_of_xs=None, to_list_of=None, output_as_string_delimiter=' '):
+def list_of_xs_converter(*,
+                         list_of_xs=None,
+                         to_list_of=None,
+                         output_as_string_delimiter=' '):
+    '''
+    list of xs converter converts a list of [lists, dicts, string lists] to another type within the same set
+    :param list_of_xs: List of xs where x is in types [lists, dicts, string lists]
+    :param to_list_of: Output type in [str, dict, list]
+    :param output_as_string_delimiter:  Delimiter for string list output, or string list input
+    :return: Converted type of list of xs
+    '''
+    if type(list_of_xs[0])== to_list_of:
+        return list_of_xs
     if type(list_of_xs[0]) == dict:
         decision_dict = {
             list:   [
@@ -31,7 +43,7 @@ def list_of_xs_converter(*, list_of_xs=None, to_list_of=None, output_as_string_d
     if type(list_of_xs[0]) == list:
         decision_dict = {
             dict:   [
-                        {list_of_xs[0][lcount]: litem for lcount, litem in enumerate(list)} 
+                        {list_of_xs[0][lcount]: litem for lcount, litem in enumerate(list)}
                         for list in list_of_xs[1:]
                     ],
             list: list_of_xs,
