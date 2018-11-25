3d56cb8d4a4445685cbec6aa82cb85cab121a76a
Erik Pohl <iotasubscript@gmail.com>
2018-11-25 17:39:37 -0500

    updated comments
    updated comments

--- python/switch_class/switch_class.py
+++ python/switch_class/switch_class.py
@@ -1,3 +1,5 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
 class switch:
     '''
     Damn the torpedoes (https://www.python.org/dev/peps/pep-3103/),
@@ -12,15 +14,35 @@ class switch:
         self.default_return_value = default_return_value
         self.decision_clauses = []
 
-    def add_switch_clause(self, switch_condition, switch_value, break_on_match):
+    def add_switch_clause(self, switch_condition, switch_result, break_on_match):
+        '''
+        add, in sequence of switching, a switch logic clause with condition,
+        result, and brake instruction 
+        :param switch_condition: The condition for which the switch result will fire
+        :param switch_result: The switch result for a given condition
+        :param break_on_match: Halt further switch evaluation on match?
+        :return: True
+        '''
         self.decision_clauses.append({'switch_condition': switch_condition,
-                                      'result': switch_value,
+                                      'result': switch_result,
                                       'break_on_match': break_on_match})
+        return True
 
     def execute_switch(self, compare_value):
+        '''
+        execute_switch
+        send a compare value through the switch to determine any results 
+        from the switch logic
+        :param compare_value: this is a value to be compared against switch logic 
+        :return: any results the compare value provoked in the switch logic
+        '''
         retval = []
-        for x in self.decision_clauses:
-            switch_condition, result, break_on_match = x['switch_condition'], x['result'], x['break_on_match']
+        for decision_clause in self.decision_clauses:
+            switch_condition,\
+            result, \
+            break_on_match = decision_clause['switch_condition'],\
+                                                       decision_clause['result'],\
+                                                       decision_clause['break_on_match']
             if compare_value in switch_condition:
                 retval.append(result)
                 if break_on_match:
