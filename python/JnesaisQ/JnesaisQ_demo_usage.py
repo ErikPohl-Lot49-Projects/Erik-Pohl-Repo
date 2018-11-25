275314bb06ec1ea8f82a4284d75cf0f70021b399
Erik Pohl <iotasubscript@gmail.com>
2018-11-25 17:34:28 -0500

    commented and fully updated
    commented and fully updated

--- python/JnesaisQ/JnesaisQ_demo_usage.py
+++ python/JnesaisQ/JnesaisQ_demo_usage.py
@@ -1,24 +1,23 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+import logging
+import re
+import sys
 from collections import namedtuple
-import logging, sys, re
 
 
-
-
-# todo : make the return value better better
-#Dict return
-#Matches
-#Mismatches
-#Conclusions:
-#Match And , or
-#Mismatch and or
 # todo : handle parameters which are attributes better
 # todo : address complicated clauses
 # (this clause AND this clause) OR (This clause AND this clause) OR (this clause)
-#Ultimately a json query grammar
-#With findings in json format
+# Ultimately a json query grammar
+# With findings in json format
+
 class JnesaisQ:
+    '''
+    JnesaisQ
+    Allows you to create a search term in JSON, and then apply that search term
+    to JSON inputs to see if and where matches or mismatches occur.
+    '''
 
     def __init__(self, JSON_query_clause):
         self.json_query_finding = namedtuple('json_query_finding', 'current_json_path, actual_finding_value')
@@ -27,8 +26,7 @@ class JnesaisQ:
         self.JSON_VALUE_MISMATCH = 'JSON_value_mismatch'
         self.JSON_query_clause = JSON_query_clause
 
-
-    def compare(self,JSON_to_query, JSON_query_clause = None,
+    def compare(self, JSON_to_query, JSON_query_clause=None,
                 *,
                 debug_mode=False,
                 current_JSON_path='',
@@ -38,10 +36,10 @@ class JnesaisQ:
         compare_json_to_query_clause
         :param JSON_to_query: This is an input JSON which you want to query
         :param JSON_query_clause: This is a clause in JSON format for the keys you want to query with values in regex
-        :param match_mode: This is the match mode to apply making the comparison [AND,OR,MISMATCH]
         :param debug_mode: Debug mode can be on or off, deprecated
         :param current_JSON_path: This is an internal only variable which tracks the current JSON path for reporting findings
-        :param JSON_query_results: This stores results of the comparison throughout the query
+        :param JnesaisQ_matches: This stores results of the matches identified by the query
+        :param JnesaisQ_mismatches: This stores results of the mismatches identified by the query
         :return: This returns findings based on the JSON_to_query, the JSON_query_clause, and the match_mode
         '''
         if not JSON_query_clause:
@@ -52,7 +50,7 @@ class JnesaisQ:
             JnesaisQ_mismatches = []
         if debug_mode:
             logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-        for format_key in self.JSON_query_clause.keys():
+        for format_key in JSON_query_clause.keys():
             current_json_path = current_JSON_path + '/' + format_key
             try:
                 JSON_to_query_key_value = JSON_to_query[format_key]
@@ -64,7 +62,7 @@ class JnesaisQ:
             ## if the format value which is being compared with the test value is itself a clause, recurse
             if isinstance(JSON_query_key_value, dict):
                 x = self.compare(JSON_to_query_key_value,
-                                 JSON_query_key_value,
+                                 JSON_query_clause=JSON_query_key_value,
                                  current_JSON_path=current_json_path,
                                  JnesaisQ_matches=JnesaisQ_matches,
                                  JnesaisQ_mismatches=JnesaisQ_mismatches,
@@ -76,7 +74,14 @@ class JnesaisQ:
                     JnesaisQ_mismatches.append(self.json_query_finding(current_json_path, self.JSON_VALUE_MISMATCH))
         return self.json_query_final_results(JnesaisQ_mismatches, JnesaisQ_matches)
 
-    def comp_bool(self, match_tuple):
+    def overall_result(self, match_tuple):
+        '''
+        overall_result
+        Returns a diagnosis of the JSON format match attempt:
+        AND_match, OR_match_mismatch, AND_mismatch
+        :param match_tuple: this is the output of JnesaisQ.compare
+        :return: an overall result
+        '''
         retval = []
         if match_tuple.json_query_mismatches == [] and match_tuple.json_query_matches == []:
             return None
@@ -88,5 +93,15 @@ class JnesaisQ:
             retval.append("AND_mismatch")
         return retval
 
-
-
+    def list_of_compares(self, list_of_JSON_to_query):
+        '''
+        list of compares
+        outputs comparisons using the current search setup for a list of JSON
+        :param list_of_JSON_to_query: this is the list of JSON to apply the query to
+        :return: list of matching JSON
+        '''
+        output_list_of_dicts = []
+        for JSON_to_query in list_of_JSON_to_query:
+            if self.overall_result(self.compare(JSON_to_query=JSON_to_query)) in ("OR_match_mismatch", "AND_match"):
+                output_list_of_dicts.append(JSON_to_query)
+        return output_list_of_dicts
