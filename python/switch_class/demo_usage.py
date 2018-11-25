e45a86a003dee48c213a13d95f4f45e04f18b2aa
Erik Pohl <iotasubscript@gmail.com>
2018-11-25 17:40:06 -0500

    updated comments
    updated comments

--- python/switch_class/demo_usage.py
+++ python/switch_class/demo_usage.py
@@ -1,3 +1,5 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
 from switch_class import switch
 
 def foo():
@@ -8,5 +10,8 @@ my_switch.add_switch_clause('2', 'Two',False)
 my_switch.add_switch_clause(['2','3'], 'Three',False)
 my_switch.add_switch_clause(foo(), 'X',False)
 my_switch.add_switch_clause('4', 'Four',True)
+my_switch.add_switch_clause('4', 'Should not get here',True)
 print(my_switch.execute_switch('1'))
 print(my_switch.execute_switch('2'))
+print(my_switch.execute_switch('5'))
+print(my_switch.execute_switch('4'))
