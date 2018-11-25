40e5df0c572fd0c8a28eb333db08454e08ece61b
Erik Pohl <iotasubscript@gmail.com>
2018-11-25 17:32:41 -0500

    cleaned up comments a bit
    cleaned up comments a bit

--- python/JnesaisQ/JnesaisQ.py
+++ python/JnesaisQ/JnesaisQ.py
@@ -1,24 +1,23 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+import logging
+import re
+import sys
 from collections import namedtuple
-import logging, sys, re
 
 
-
-
-# todo : make the return value better better
-#Dict return
-#Matches
-#Mismatches
-#Conclusions:
-#Match And , or
-#Mismatch and or
 # todo : handle parameters which are attributes better
 # todo : address complicated clauses
 # (this clause AND this clause) OR (This clause AND this clause) OR (this clause)
-#Ultimately a json query grammar
-#With findings in json format
+# Ultimately a json query grammar
+# With findings in json format
+
 class JnesaisQ:
+    '''
+    JnesaisQ 
+    Allows you to create a search term in JSON, and then apply that search term
+    to JSON inputs to see if and where matches or mismatches occur.
+    '''
 
     def __init__(self, JSON_query_clause):
         self.json_query_finding = namedtuple('json_query_finding', 'current_json_path, actual_finding_value')
@@ -27,8 +26,7 @@ class JnesaisQ:
         self.JSON_VALUE_MISMATCH = 'JSON_value_mismatch'
         self.JSON_query_clause = JSON_query_clause
 
-
-    def compare(self,JSON_to_query, JSON_query_clause = None,
+    def compare(self, JSON_to_query, JSON_query_clause=None,
                 *,
                 debug_mode=False,
                 current_JSON_path='',
@@ -38,10 +36,10 @@ class JnesaisQ:
         compare_json_to_query_clause
         :param JSON_to_query: This is an input JSON which you want to query
         :param JSON_query_clause: This is a clause in JSON format for the keys you want to query with values in regex
-        :param match_mode: This is the match mode to apply making the comparison [AND,OR,MISMATCH]
         :param debug_mode: Debug mode can be on or off, deprecated
         :param current_JSON_path: This is an internal only variable which tracks the current JSON path for reporting findings
-        :param JSON_query_results: This stores results of the comparison throughout the query
+        :param JnesaisQ_matches: This stores results of the matches identified by the query
+        :param JnesaisQ_mismatches: This stores results of the mismatches identified by the query
         :return: This returns findings based on the JSON_to_query, the JSON_query_clause, and the match_mode
         '''
         if not JSON_query_clause:
@@ -77,6 +75,13 @@ class JnesaisQ:
         return self.json_query_final_results(JnesaisQ_mismatches, JnesaisQ_matches)
 
     def overall_result(self, match_tuple):
+        '''
+        overall_result
+        Returns a diagnosis of the JSON format match attempt:
+        AND_match, OR_match_mismatch, AND_mismatch
+        :param match_tuple: this is the output of JnesaisQ.compare
+        :return: an overall result
+        '''
         retval = []
         if match_tuple.json_query_mismatches == [] and match_tuple.json_query_matches == []:
             return None
@@ -87,14 +92,16 @@ class JnesaisQ:
         if match_tuple.json_query_mismatches and not match_tuple.json_query_matches:
             retval.append("AND_mismatch")
         return retval
-    
+
     def list_of_compares(self, list_of_JSON_to_query):
+        '''
+        list of compares
+        outputs comparisons using the current search setup for a list of JSON 
+        :param list_of_JSON_to_query: this is the list of JSON to apply the query to
+        :return: list of matching JSON
+        '''
         output_list_of_dicts = []
         for JSON_to_query in list_of_JSON_to_query:
-            if self.overall_result(self.compare(JSON_to_query=JSON_to_query)) in ("OR_match_mismatch","AND_match" ):
+            if self.overall_result(self.compare(JSON_to_query=JSON_to_query)) in ("OR_match_mismatch", "AND_match"):
                 output_list_of_dicts.append(JSON_to_query)
         return output_list_of_dicts
-                
-
-
-
