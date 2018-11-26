67d5ff5a8923b3afb685dc802dc2415ca6a33c3b
Erik Pohl <iotasubscript@gmail.com>
2018-11-25 22:37:47 -0500

    pep8 linted it
    pep8 linted it

--- python/list_of_xs_sorter/list_of_string_lists_sorter.py
+++ python/list_of_xs_sorter/list_of_string_lists_sorter.py
@@ -1,20 +1,34 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
 from collections import namedtuple
 from datetime import date
 from list_of_lists_sorter import list_of_lists_sorter
 from list_of_xs_converter import list_of_xs_converter
 
-#TODO: infer delimiter?
-#TODO: add regression tests
+__author__ = "Erik Pohl"
+__copyright__ = "None"
+__credits__ = ["Erik Pohl"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Erik Pohl"
+__email__ = "erik.pohl.444@gmail.com"
+__status__ = "Beta"
+
+
+# TODO: infer delimiter?
+# TODO: add regression tests
+
+
 class list_of_string_lists_sorter:
     '''
-    This class allows you to flexibly define and execute sorts on lists of string lists
+    This class allows you to flexibly define and execute sorts
+    on lists of string lists
     with and without headers
     with and without reverse sort
     '''
     sort_field = namedtuple('sort_field', 'position field_type')
 
     STRINGDATEPREFIX = 'datestringdelimiter'
-
 
     def __init__(self, list_of_string_lists, string_list_delimiter):
         self.list_of_string_lists = list_of_string_lists
@@ -24,39 +38,69 @@ class list_of_string_lists_sorter:
         self.reverse_sort = False
         self.has_header = False
 
+    def add_sort_field_by_position(
+            self, column_position, sort_field_type="string"
+    ):
+        '''
+        add a sort field to the sort field criteria by position in the list,
+        sort field type default is string
+        '''
+        self.sort_fields.append(
+            self.sort_field(
+                column_position,
+                sort_field_type
+            )
+        )
 
-    def add_sort_field_by_position(self, column_position, sort_field_type="string"):
+    def add_sort_field_by_header_field_name(
+            self, field_name, sort_field_type="string"
+    ):
         '''
-        add a sort field to the sort field criteria by position in the list, sort field type default is string
-        '''
-        self.sort_fields.append(self.sort_field(column_position, sort_field_type))
-
-    def add_sort_field_by_header_field_name(self, field_name, sort_field_type="string"):
-        '''
-        add a sort field to the sort field criteria by header field name in the list, sort field type default is string
+        add a sort field to the sort field criteria by header field
+        name in the list, sort field type default is string
         '''
         if self.has_header:
-            self.sort_fields.append(self.sort_field(self.list_of_string_lists[0].split(self.string_list_delimiter)
-                                                    .index(field_name), sort_field_type))
+            self.sort_fields.append(
+                self.sort_field(
+                    self.list_of_string_lists[0].split(
+                        self.string_list_delimiter
+                    ).index(field_name),
+                    sort_field_type
+                )
+            )
 
     def add_multiple_fields_by_position(self, position_type_list):
         '''
-        add a multiple sort fields to the sort field criteria by position in the list, sort field type default is string
+        add a multiple sort fields to the sort field criteria
+        by position in the list, sort field type default is string
         '''
         for sort_field_add in position_type_list:
             if type(sort_field_add) is not tuple:
                 sort_field_add = (sort_field_add, "string")
-            self.sort_fields.append(self.sort_field(sort_field_add[0], sort_field_add[1]))
+            self.sort_fields.append(
+                self.sort_field(
+                    sort_field_add[0],
+                    sort_field_add[1]
+                )
+            )
 
     def add_multiple_fields_by_header_field(self, header_type_list):
         '''
-        add a multiple sort fields to the sort field criteria by header field name in the list, sort field type default is string
+        add a multiple sort fields to the sort field criteria
+        by header field name in the list, sort field type default is string
         '''
         for sort_field_add in header_type_list:
             if type(sort_field_add) is not tuple:
                 sort_field_add = (sort_field_add, "string")
-            self.sort_fields.append(self.sort_field(self.list_of_lists[0].split(self.string_list_delimiter)
-                                                    .index(sort_field_add[0]), sort_field_add[1]))
+            self.sort_fields.append(
+                self.sort_field(
+                    self.list_of_lists[0].split(
+                        self.string_list_delimiter
+                    ).index(
+                        sort_field_add[0]
+                    ), sort_field_add[1]
+                )
+            )
 
     def clear_sort_fields(self):
         '''
@@ -75,14 +119,23 @@ class list_of_string_lists_sorter:
 
     def sort(self):
         '''
-        execute the sort based on all of the criteria and setups in the instantiation
+        execute the sort based on all of the criteria
+        and setups in the instantiation
         '''
-        xvert_list = [i.split(self.string_list_delimiter) for i in self.list_of_string_lists]
+        xvert_list = [
+            string_list.split(self.string_list_delimiter)
+            for string_list in self.list_of_string_lists
+        ]
         self._list_of_lists_sorter = list_of_lists_sorter(xvert_list)
         self._list_of_lists_sorter.has_header = self.has_header
         self._list_of_lists_sorter.reverse_sort = self.reverse_sort
         self._list_of_lists_sorter.sort_fields = self.sort_fields
-        unxvert_list = [self.string_list_delimiter.join(i) for i in self._list_of_lists_sorter.sort()]
-        return list_of_xs_converter(list_of_xs=unxvert_list,
-                                    to_list_of=self.output_as_list_of,
-                                    output_as_string_delimiter= self.string_list_delimiter)
+        unxvert_list = [
+            self.string_list_delimiter.join(i)
+            for i in self._list_of_lists_sorter.sort()
+        ]
+        return list_of_xs_converter(
+            list_of_xs=unxvert_list,
+            to_list_of=self.output_as_list_of,
+            output_as_string_delimiter=self.string_list_delimiter
+        )
