3e4a01ef4cb348f71859d1e4af19b667cab06a81
Erik Pohl <iotasubscript@gmail.com>
2018-12-02 13:20:49 -0500

    added comments
    added comments

--- python/RestSemblance/MyTests.py
+++ python/RestSemblance/MyTests.py
@@ -7,10 +7,19 @@ from Semblance import startCaptureOutput, stopCapturedOutput
 
 
 class MyTestCases(Semblance):
-    # change location of get command
+    '''
+    Demo usage for Semblance
+    '''
     @mock.patch('sample_client_in_need_of_mocking.requests.get',
                 side_effect=semblance_mocked_requests_get)
     def testWithMockValidJSon(self, *args, **kwargs):
+        '''
+        test incremeneting through test cases
+        using mocked endpoints
+        :param args: not needed
+        :param kwargs: not needed
+        :return: True
+        '''
         try:
             while True:
                 print("Incremented to ", Semblance.currentcase)
@@ -18,8 +27,15 @@ class MyTestCases(Semblance):
                 inccurrentcase()
         except:
             pass
+        return True
 
-    def test2(self, *args, **kwargs):
+    def test_capture_stdout(self, *args, **kwargs):
+        '''
+        tests capturing stdout output using Semblance
+        :param args: not needed
+        :param kwargs: not needed
+        :return: captured output
+        '''
         startCaptureOutput()
         print("hello")
         capout = stopCapturedOutput()
