8a5a3a75a935035cb60c5b484d0b6d05d444800e
Erik Pohl <iotasubscript@gmail.com>
2018-12-02 13:00:16 -0500

    reformatted based on linting
    PEP8

--- python/RestSemblance/sample_client_in_need_of_mocking.py
+++ python/RestSemblance/sample_client_in_need_of_mocking.py
@@ -5,18 +5,24 @@ url_base = 'http://127.0.0.1:5002'
 url_students = url_base + '/Students'
 url_courses = url_base + '/Courses'
 
+
+def output_response(status_code, text):
+    print(status_code, text)
+    print(
+        "status_code, text -->",
+        status_code,
+        text
+    )
+
+
+def perform_request(api_url):
+    r_get_response = requests.get(url=api_url)
+    output_response(r_get_response.status_code, r_get_response.text)
+
+
 def GetStudentAndCourse():
-    api_url = url_students
-    r_get_student = requests.get(url=api_url)
-    print(r_get_student.status_code, r_get_student.text)
-    print("status_code, text -->", r_get_student.status_code, r_get_student.text)
-
-    api_url = url_courses
-    r_get_course = requests.get(url=api_url)
-    print(r_get_course.status_code, r_get_course.text)
-    print("status_code, text -->", r_get_course.status_code, r_get_course.text)
-    return r_get_student.text, r_get_course.text
-
+    perform_request(url_students)
+    perform_request(url_courses)
 
 if __name__ == '__main__':
     GetStudentAndCourse()
