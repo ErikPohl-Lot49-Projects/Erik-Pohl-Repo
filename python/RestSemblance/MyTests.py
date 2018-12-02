0f652a5a03dcbbc7420cd54e125ddb95ba9ebd12
Erik Pohl <iotasubscript@gmail.com>
2018-12-02 12:47:04 -0500

    reformatted code according to linting
    PEP8

--- python/RestSemblance/MyTests.py
+++ python/RestSemblance/MyTests.py
@@ -1,13 +1,16 @@
+from unittest import mock
+import logging
+import sys
+from sample_client_in_need_of_mocking import GetStudentAndCourse
 from Semblance import Semblance, semblance_mocked_requests_get, inccurrentcase
 from Semblance import startCaptureOutput, stopCapturedOutput
-from unittest import mock
-import logging, sys
-from sample_client_in_need_of_mocking import GetStudentAndCourse
+
 
 class MyTestCases(Semblance):
     # change location of get command
-    @mock.patch('sample_client_in_need_of_mocking.requests.get', side_effect=semblance_mocked_requests_get)
-    def testWithMockValidJSon(self,*args, **kwargs):
+    @mock.patch('sample_client_in_need_of_mocking.requests.get',
+                side_effect=semblance_mocked_requests_get)
+    def testWithMockValidJSon(self, *args, **kwargs):
         try:
             while True:
                 print("Incremented to ", Semblance.currentcase)
@@ -16,12 +19,12 @@ class MyTestCases(Semblance):
         except:
             pass
 
-    def test2(self,*args, **kwargs):
+    def test2(self, *args, **kwargs):
         startCaptureOutput()
         print("hello")
         capout = stopCapturedOutput()
         print("captured -->", capout)
-        self.assertEqual("hello\n",capout)
+        self.assertEqual("hello\n", capout)
 
     # set up logging for all cases
     logging.basicConfig(stream=sys.stderr, level=logging.INFO)
