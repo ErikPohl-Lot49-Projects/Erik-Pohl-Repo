bc740ee9f6c5e6a3d7c8ec6f011e91a4fb245fa0
Erik Pohl <iotasubscript@gmail.com>
2018-12-02 13:23:28 -0500

    added comments
    added comments

--- python/RestSemblance/sample_client_in_need_of_mocking.py
+++ python/RestSemblance/sample_client_in_need_of_mocking.py
@@ -7,22 +7,40 @@ url_courses = url_base + '/Courses'
 
 
 def output_response(status_code, text):
+    '''
+    output results of requests get
+    :param status_code: status code
+    :param text: text of request get
+    :return: True
+    '''
     print(status_code, text)
     print(
         "status_code, text -->",
         status_code,
         text
     )
+    return True
 
 
 def perform_request(api_url):
+    '''
+    This will be mocked
+    :param api_url: url to retrieve API data from using requests get
+    :return:
+    '''
     r_get_response = requests.get(url=api_url)
     output_response(r_get_response.status_code, r_get_response.text)
+    return True
 
 
 def GetStudentAndCourse():
+    '''
+    API requests to get Student and Course data
+    :return: True
+    '''
     perform_request(url_students)
     perform_request(url_courses)
+    return True
 
 if __name__ == '__main__':
     GetStudentAndCourse()
