d93376121e3820b592e87a821ca32a9890d08d29
Erik Pohl <iotasubscript@gmail.com>
2018-12-02 13:03:31 -0500

    reformatted to PEP8
    after linting

--- python/RestSemblance/MakeTestCaseFile.py
+++ python/RestSemblance/MakeTestCaseFile.py
@@ -1,33 +1,30 @@
 import pickle
 
-endpointdatasource = {'TestCase1':
-    {
-        'http://127.0.0.1:5002/Students':
-            {
+endpointdatasource = {'TestCase1': {
+        'http://127.0.0.1:5002/Students': {
                 'text': '[{"Student":1},{"hello": 1}]',
                 'status_code': '200'
             },
-        'http://127.0.0.1:5002/Courses':
-            {
+        'http://127.0.0.1:5002/Courses': {
                 'text': '[{"Course":1},{"hello": 1}]',
                 'status_code': '200'
             }
-    }
-    ,
-    'TestCase2':
-        {
-            'http://127.0.0.1:5002/Students':
-                {
+    },
+    'TestCase2': {
+            'http://127.0.0.1:5002/Students': {
                     'text': '[{"Student":2},{"hello": 1}]',
                     'status_code': '200'
                 },
-            'http://127.0.0.1:5002/Courses':
-                {
+            'http://127.0.0.1:5002/Courses': {
                     'text': '[{"Course":2},{"hello": 1}]',
                     'status_code': '200'
                 }
         }
 }
 
-with open("TestCaseFile.pickle","wb") as TestCaseFileHandle:
-    TestCaseFileHandle.write(pickle.dumps(endpointdatasource))
+with open("TestCaseFile.pickle", "wb") as TestCaseFileHandle:
+    TestCaseFileHandle.write(
+        pickle.dumps(
+            endpointdatasource
+        )
+    )
