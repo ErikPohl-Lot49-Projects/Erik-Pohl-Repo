bf4f0b00ed77bf892b06c6489327afe1f49a7558
Erik Pohl <iotasubscript@gmail.com>
2018-12-02 13:18:13 -0500

    added comments
    added comments

--- python/RestSemblance/Semblance.py
+++ python/RestSemblance/Semblance.py
@@ -11,9 +11,17 @@ from SemblanceExceptions import UnrecognizedURLTestCase
 # TODO:         Route faked DB / file?
 # TODO:         mock function calls and such?
 # TODO: check out references for inspiration for project growth
+# TODO: allow changing file name
+# TODO: consider liberating test cases from fixed nomenclature
 
 
 def semblance_mocked_requests_get(*args, **kwargs):
+    '''
+    Get the response data for the mocked endpoint
+    :param args: positional arguments to the mock side-effect
+    :param kwargs: keyword arguments to the mock side-effect
+    :return: response or error
+    '''
     currentcase = TestCase.currentcase
     endpointdata = TestCase.endpointdatasource
     if kwargs:
@@ -28,9 +36,15 @@ def semblance_mocked_requests_get(*args, **kwargs):
             "Did not recognize the URL to be mocked: " + kwargs['url']
         )
         raise UnrecognizedURLTestCase
+    return True
 
 
 def inccurrentcase():
+    '''
+    Increment test cases using
+    a standard nomenclature
+    :return: True
+    '''
     # do this far more gracefully
     TestCase.counter += 1
     TestCase.currentcase = 'TestCase' + str(TestCase.counter)
@@ -38,19 +52,36 @@ def inccurrentcase():
         TestCase.endpointdatasource[TestCase.currentcase]
     except:
         raise StopIteration
+    return True
 
 
 def startCaptureOutput():
+    '''
+    Start capturing stdout output
+    :return: True
+    '''
     TestCase.capturedOutput = io.StringIO()
     sys.stdout = TestCase.capturedOutput
+    return True
 
 
 def stopCapturedOutput():
+    '''
+    End capturing stdout output
+    and return it
+    :return: captured stdout output
+    '''
     sys.stdout = sys.__stdout__
     return TestCase.capturedOutput.getvalue()
 
 
 def LoadCases():
+    '''
+    Loads test cases from a pickled dictionary
+    Set current test case to first test case
+    TestCaseFile.pickle
+    :return: True
+    '''
     TestCase.counter = 1
     TestCase.currentcase = 'TestCase' + str(TestCase.counter)
     # consider loading expected results so that it can be compared
@@ -59,6 +90,7 @@ def LoadCases():
         TestCase.endpointdatasource = pickle.loads(
             Test_Case_File_Handle.read()
         )
+    return True
 
 
 class Semblance(TestCase):
