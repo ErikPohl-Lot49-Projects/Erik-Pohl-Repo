7ecbdf412848ebc5ac31314f3481d97695cd80fa
Erik Pohl <iotasubscript@gmail.com>
2018-12-03 18:06:48 -0500

    added some comments and functionality
    added some comments and functionality

--- python/RestSemblance/SemblanceExceptions.py
+++ python/RestSemblance/SemblanceExceptions.py
@@ -1,7 +1,26 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Erik Pohl"
+__copyright__ = "None"
+__credits__ = ["Erik Pohl"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Erik Pohl"
+__email__ = "erik.pohl.444@gmail.com"
+__status__ = "Beta"
+
+
+# TODO: on sunday make this output messages
+# https://www.codementor.io/sheena/how-to-write-python-custom-exceptions-du107ufv9
+#
 class Error(Exception):
-   """Base class for other exceptions"""
-   pass
+    """Base class for other exceptions"""
 
 class UnrecognizedURLTestCase(Error):
-   """Raised when a mocked API call function uses an unknown endpoint"""
-   pass
+    """Raised when a mocked API call function uses an unknown endpoint"""
+    def __init__(self, msg=None):
+        if msg is None:
+            # Set some default useful error message
+            msg = 'this was an unrecognized test case!'
+        print(msg)
