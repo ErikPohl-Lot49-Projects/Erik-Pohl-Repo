743d0bbc54e299d8b838292534d9614dc5a68009
Erik Pohl <iotasubscript@gmail.com>
2018-12-02 23:31:10 -0500

    linted it with pep8
    linted it with pep8

--- python/guarantee_decorator/demo_usage.py
+++ python/guarantee_decorator/demo_usage.py
@@ -2,20 +2,46 @@
 # -*- coding: utf-8 -*-
 from guarantee import guarantee
 
+__author__ = "Erik Pohl"
+__copyright__ = "None"
+__credits__ = ["Erik Pohl"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Erik Pohl"
+__email__ = "erik.pohl.444@gmail.com"
+__status__ = "Beta"
+
+
 @guarantee('n')
 def fibonacci_keywordarg_guarantee(**kwargs):
-    """uses guarantee decorator to guarantee a particular variable name is used with keyword
+    """uses guarantee decorator to guarantee a particular
+    variable name is used with keyword
     arguments passed into an open-ended keyword argument"""
-    if kwargs['n'] in (0,1):
-        return kwargs['n']
-    return (fibonacci_keywordarg_guarantee(n=kwargs['n']-2) + fibonacci_keywordarg_guarantee(n=kwargs['n']-1))
+    nput = kwargs['n']
+    if nput in (0, 1):
+        return nput
+    return (
+            fibonacci_keywordarg_guarantee(
+                n=nput-2
+            ) +
+            fibonacci_keywordarg_guarantee(
+                n=nput-1
+            )
+    )
+
 
 def fibonacci_keywordarg_standard_mandatory(*, n):
     """standard methodology to force use of the n keyword argument"""
-    if n in (0,1):
+    if n in (0, 1):
         return n
-    return (fibonacci_keywordarg_standard_mandatory(n=n-2) + fibonacci_keywordarg_standard_mandatory(n=n-1))
+    return (
+            fibonacci_keywordarg_standard_mandatory(n=n-2) +
+            fibonacci_keywordarg_standard_mandatory(n=n-1)
+    )
 
-
-print(fibonacci_keywordarg_guarantee(n=9))
-print(fibonacci_keywordarg_standard_mandatory(n=9))
+print(
+    fibonacci_keywordarg_guarantee(n=9)
+)
+print(
+    fibonacci_keywordarg_standard_mandatory(n=9)
+)
