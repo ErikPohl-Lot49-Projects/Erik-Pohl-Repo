25b4117e9b5cebfafa7a5a45f49365cba0c13173
Erik Pohl <iotasubscript@gmail.com>
2018-12-02 23:30:46 -0500

    linted it with pep8
    linted it with pep8

--- python/guarantee_decorator/guarantee.py
+++ python/guarantee_decorator/guarantee.py
@@ -4,22 +4,26 @@ from functools import wraps
 
 # TODO potentially Guarantee will use regex on variable values for validation
 
+
 def guarantee(*argsx):
     """"this is a decorator to guarantee specific keyword arguments are used
     in an otherwise open ended keyword argument list
-    Allows for extra messaging and handling for an error related to missing or invalid keyword arguments
+    Allows for extra messaging and handling for an error
+    related to missing or invalid keyword arguments
     """
     def real_decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             if list(argsx) != list(kwargs.keys()) and kwargs != {}:
-                print("expected argument list [%s] does not match argument list [%s]" % (argsx, list(kwargs.keys())))
+                print(
+                    "expected argument list [%s] "
+                    "does not match argument list [%s]" % (
+                        argsx, list(kwargs.keys()
+                                    )
+                    )
+                )
                 raise Exception("expected argument does not match argument")
             result = func(*args, **kwargs)
             return result
         return wrapper
     return real_decorator
-
-
-
-
