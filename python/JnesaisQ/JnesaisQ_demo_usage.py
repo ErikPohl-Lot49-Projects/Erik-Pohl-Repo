a3547006ab29cc741ae9ab3ddcf7100c41285bd5
Erik Pohl <iotasubscript@gmail.com>
2018-12-03 00:07:02 -0500

    reformatted based on pep8
    reformatted based on pep8

--- python/JnesaisQ/JnesaisQ_demo_usage.py
+++ python/JnesaisQ/JnesaisQ_demo_usage.py
@@ -3,9 +3,18 @@
 from json import loads
 from JnesaisQ import JnesaisQ
 
+__author__ = "Erik Pohl"
+__copyright__ = "None"
+__credits__ = ["Erik Pohl"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Erik Pohl"
+__email__ = "erik.pohl.444@gmail.com"
+__status__ = "Beta"
 
 
-test_json_str =         '{"hello": "1", "zap": {"h1": "one", "h2": "two", "single": "."}}'
+test_json_str = '{"hello": "1", "zap": {"h1": ' \
+                '"one", "h2": "two", "single": "."}}'
 json_query_format_str = '{"zap": {"h1": ".*"}, "hello": ".*"}'
 test_json = loads(test_json_str)
 json_query_format = loads(json_query_format_str)
@@ -17,4 +26,3 @@ result = JNSQ.compare(test_json,  debug_mode=0)
 print("mismatches", result.json_query_mismatches)
 print("matches", result.json_query_matches)
 print("Overall result", JNSQ.overall_result(result))
-
