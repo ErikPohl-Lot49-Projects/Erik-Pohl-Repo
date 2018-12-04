42cfafdaa83b7326649c9d7ce0c3d4c25f5caa87
Erik Pohl <iotasubscript@gmail.com>
2018-12-03 23:18:54 -0500

    more complicated dictionary
    more complicated dictionary

--- python/RestSemblance/MakeTestCaseFile.py
+++ python/RestSemblance/MakeTestCaseFile.py
@@ -1,10 +1,22 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
 import pickle
 
-# endpointdatasource ideally would come from a front-end application geared
+__author__ = "Erik Pohl"
+__copyright__ = "None"
+__credits__ = ["Erik Pohl"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Erik Pohl"
+__email__ = "erik.pohl.444@gmail.com"
+__status__ = "Beta"
+
+# TODO: endpointdatasource ideally would come from a front-end application geared
 # towards prompting non-technical folks
 # to construct test cases for each API endpoint
 
 endpointdatasource = {'TestCase1': {
+    'urls': {
         'http://127.0.0.1:5002/Students': {
                 'text': '[{"Student":1},{"hello": 1}]',
                 'status_code': '200'
@@ -14,7 +26,12 @@ endpointdatasource = {'TestCase1': {
                 'status_code': '200'
             }
     },
+    'args': [1,2],
+    'kwargs': {'c':3},
+    'Expected_Output': True
+    },
     'TestCase2': {
+            'urls': {
             'http://127.0.0.1:5002/Students': {
                     'text': '[{"Student":2},{"hello": 1}]',
                     'status_code': '200'
@@ -23,6 +40,11 @@ endpointdatasource = {'TestCase1': {
                     'text': '[{"Course":2},{"hello": 1}]',
                     'status_code': '200'
                 }
+            },
+    'args': [1,2],
+    'kwargs': {'c':3},
+    'Expected_Output': True
+
         }
 }
 
