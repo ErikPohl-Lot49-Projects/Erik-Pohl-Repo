fe879b04daf0774abdfe1ea994f8e6b21543f5a4
Erik Pohl <iotasubscript@gmail.com>
2018-12-04 23:15:32 -0500

    added more functionality
    added more functionality

--- python/guarantee_decorator/guarantee.py
+++ python/guarantee_decorator/guarantee.py
@@ -1,11 +1,14 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 from functools import wraps
+import re
 
-# TODO potentially Guarantee will use regex on variable values for validation
+# TODO: can this be combined into one?
+# TODO: regex assumes string or byte types-- so won't work for various inputs
+# TODO: type checking of input variables?
+# TODO: support function validation (lambda and otherwise)
 
-
-def guarantee(*argsx):
+def guarantee_regex(*argsx):
     """"this is a decorator to guarantee specific keyword arguments are used
     in an otherwise open ended keyword argument list
     Allows for extra messaging and handling for an error
@@ -14,7 +17,37 @@ def guarantee(*argsx):
     def real_decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            if list(argsx) != list(kwargs.keys()) and kwargs != {}:
+            #print(argsx,kwargs)
+            try:
+                # attempt 1, regex and key value match at same time
+                [re.match(str(list(argument.values())[0]),str(kwargs[next(iter(argument))])) for argument in argsx]
+            except:
+                print(
+                    "expected argument list [%s] "
+                    "does not match argument list [%s]" % (
+                        argsx, list(kwargs.keys()
+                                    )
+                    )
+                )
+                raise Exception("expected argument does not match argument")
+            result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return real_decorator
+
+def guarantee_variable_names(*argsx):
+    """"this is a decorator to guarantee specific keyword arguments are used
+    in an otherwise open ended keyword argument list
+    Allows for extra messaging and handling for an error
+    related to missing or invalid keyword arguments
+    """
+    def real_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            #print(argsx,kwargs)
+            try:
+                [kwargs[next(iter(argument))] for argument in argsx]
+            except:
                 print(
                     "expected argument list [%s] "
                     "does not match argument list [%s]" % (
