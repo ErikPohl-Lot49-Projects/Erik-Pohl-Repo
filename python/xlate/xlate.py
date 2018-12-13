9e327cbf46b47421bb9d00938393d773346ab5fb
Erik Pohl <iotasubscript@gmail.com>
2018-12-13 15:02:51 -0500

    aha
    aha

--- python/xlate/xlate.py
+++ python/xlate/xlate.py
@@ -6,22 +6,37 @@ Created on Dec 13, 2018
 @author: Erik Pohl
 '''
 
-from xlate import xlate
-
-input_string = 'pohl erik 9/2/72 python arlington'
-input_del = ' '
-input_format = ['lname', 'fname', 'bdate', 'language_of_choice', 'hometown']
-output_format = '{fname} {lname}, born on {bdate}, lives in {hometown} and prefers {language_of_choice}'
-
-demo_usage = xlate(input_string, input_del, input_format)
-print(demo_usage.xlate_to_str(output_format))
-print(demo_usage.xlate_to_dict())
-
-output_format = '{0} {1}, born on {2}, lives in {3} and prefers {4}'
-demo_usage = xlate(input_string, input_del)
-print(demo_usage.xlate_to_str(output_format))
-print(demo_usage.xlate_to_dict())
+class xlate:
+    
+    def __init__(self, xlate_input, xlate_delimiter, xlate_input_format=None):
+        self.input = xlate_input
+        self.input_delimiter = xlate_delimiter
+        self._input_list = self.input.split(self.input_delimiter)
+        self.input_format = xlate_input_format
 
 
-
-
+    def xlate_to_str(self, output_format):
+        '''
+        takes an input, splits it by delimiter, applies a list of columns in the input_format,
+        and returns fields in a string based on the output_format
+        works on positional and non positional output
+        '''
+        if self.input_format:
+            return output_format.format(**self.xlate_to_dict())
+        else:
+            return (output_format.format(*self._input_list))
+    
+    def xlate_to_dict(self):
+        '''
+        takes an input, splits it by delimiter, applies a list of columns in the input_format
+        or numbers if no input format,
+        and returns fields in a dict
+        '''
+        return {
+            k:v for (k,v) in zip(
+                self.input_format if self.input_format else [
+                    str(x) for x,_ in enumerate(self._input_list)
+                    ], 
+                self._input_list
+                )
+            }
