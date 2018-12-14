88e7ebbbd486ab6a7f6ab24412dfe5203e5e0cfa
Erik Pohl <iotasubscript@gmail.com>
2018-12-13 21:19:29 -0500

    change
    change

--- python/xlate/xlate_demo_usage.py
+++ python/xlate/xlate_demo_usage.py
@@ -14,13 +14,13 @@ input_format = ['lname', 'fname', 'bdate', 'language_of_choice', 'hometown']
 output_format = '{fname} {lname}, born on {bdate}, lives in {hometown} and prefers {language_of_choice}'
 
 demo_usage = xlate(input_string, input_del, input_format)
-print(demo_usage.xlate_to_str(output_format))
-print(demo_usage.xlate_to_dict())
+print(demo_usage.to_string_using_keyword_format(output_format))
+print(demo_usage.to_dictionary())
 
 output_format = '{0} {1}, born on {2}, lives in {3} and prefers {4}'
 demo_usage = xlate(input_string, input_del)
-print(demo_usage.xlate_to_str(output_format))
-print(demo_usage.xlate_to_dict())
+print(demo_usage.to_string_using_keyword_format(output_format))
+print(demo_usage.to_dictionary())
 
 output_format = '{fname} {lname}, born on {bdate}, lives in {hometown} and prefers {language_of_choice}'
-print(demo_usage.xlate_to_str_pos_override(output_format))
+print(demo_usage.to_string_forcing_positional(output_format))
