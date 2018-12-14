17300e6ee9642d67f50845f61a2176a87dcd19f4
Erik Pohl <iotasubscript@gmail.com>
2018-12-13 20:57:37 -0500

    added positional override
    needs refactoring in a bad way

--- python/xlate/xlate.py
+++ python/xlate/xlate.py
@@ -6,17 +6,35 @@ Created on Dec 13, 2018
 @author: Erik Pohl
 '''
 
+
 class xlate:
-    
+
     def __init__(self, xlate_input, xlate_delimiter, xlate_input_format=None):
         self.input = xlate_input
         self.input_delimiter = xlate_delimiter
         self._input_list = self.input.split(self.input_delimiter)
         self.input_format = xlate_input_format
 
-
-    def convert_to_positional(self, astring):         
-        return astring
+    def convert_to_positional(self, key_field_output_format):
+        '''
+        converts an output format into positional from field names
+        :param key_field_output_format: output format
+        :return:
+        '''
+        positional_output_format = ''
+        first_index = None
+        last_index = 0
+        counter = 0
+        for char_index, character in enumerate(key_field_output_format):
+            if character == '{':
+                first_index = char_index
+            if character == '}' and first_index is not None:
+                positional_output_format += key_field_output_format[last_index:first_index + 1] + str(counter)
+                counter += 1
+                last_index = char_index
+                first_index = None
+        positional_output_format += key_field_output_format[last_index:]
+        return positional_output_format
 
     def xlate_to_str_pos_override(self, output_format):
         '''
@@ -24,8 +42,7 @@ class xlate:
         and returns fields in a string based on the output_format
         works on positional and non positional output
         '''
-        self.convert_to_positional(output_format)
-        return (output_format.format(*self._input_list))
+        return (self.convert_to_positional(output_format).format(*self._input_list))
 
     def xlate_to_str(self, output_format):
         '''
@@ -37,7 +54,7 @@ class xlate:
             return output_format.format(**self.xlate_to_dict())
         else:
             return (output_format.format(*self._input_list))
-    
+
     def xlate_to_dict(self):
         '''
         takes an input, splits it by delimiter, applies a list of columns in the input_format
@@ -45,10 +62,10 @@ class xlate:
         and returns fields in a dict
         '''
         return {
-            k:v for (k,v) in zip(
-                self.input_format if self.input_format else [
-                    str(x) for x,_ in enumerate(self._input_list)
-                    ], 
-                self._input_list
-                )
-            }
+            k: v for (k, v) in zip(
+            self.input_format if self.input_format else [
+                str(x) for x, _ in enumerate(self._input_list)
+            ],
+            self._input_list
+        )
+        }
