80ed38f6a3ec489c884ce3609d1862ffcf56f12a
Erik Pohl <iotasubscript@gmail.com>
2018-12-14 10:53:45 -0500

    added return dict
    with version check

--- python/Countput/Countput.py
+++ python/Countput/Countput.py
@@ -3,9 +3,17 @@ Created on Dec 14, 2018
 
 @author: Erik Pohl
 '''
-from collections import Counter
+from collections import Counter, OrderedDict
+import sys
+import re
+from wsgiref.simple_server import sys_version
 
 class Countput(Counter):
+    
+    def mycmp(self,version1, version2):
+        def normalize(v):
+            return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+        return normalize(version1) >=  normalize(version2) 
     
  
     def return_list(self, n = None, delimiter = ' '):
@@ -16,3 +24,12 @@ class Countput(Counter):
     def output_topn(self, n = None, delimiter = ' '):
         [print(delimiter.join([str(x) for x in i])) for i in self.most_common(n)]        
 
+    def return_dict(self):
+        # do something different for versions of Python
+        # where the dictionary is not automatically ordered
+        if self.mycmp('.'.join((str(sys.version_info.major),str(sys.version_info.minor), str(sys.version_info.micro))),"3.7.1"):
+            return_dictionary = {}
+        else:  
+            return_dictionary = OrderedDict()   
+        return_dictionary.update({frequency_tuple[0]:frequency_tuple[1] for frequency_tuple in self.most_common()})
+        return return_dictionary
