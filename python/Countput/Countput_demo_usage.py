e62673845f7b5b0e8f71f4c72a651554906b6a99
Erik Pohl <iotasubscript@gmail.com>
2018-12-15 17:07:27 -0500

    headless output
    madame guillotine

--- python/Countput/Countput_demo_usage.py
+++ python/Countput/Countput_demo_usage.py
@@ -11,9 +11,15 @@ word_list = word_sentence.split(' ')
 
 
 MyCountput = Countput(word_list)
-print('return_as_list of strings : ' + str(MyCountput.return_topn_as_list_of_strings(2, ' - Value', '[Term: ', ' ]','Header' )))
+print('return_as_list of strings : ' + str(MyCountput.return_topn_as_list_of_strings(n=2,
+                                                                                     delimiter=' - Value',
+                                                                                     prefix='[Term: ',
+                                                                                     suffix=' ]',
+                                                                                     header='Header' )))
 print('return_as_list of strings for sort_as_list_of_strings: ' +
-      str(MyCountput.return_topn_as_list_of_strings(2, ',', '', '', 'Term,Count' )))
-
+      str(
+          MyCountput.return_topn_as_list_of_strings(n=2)
+      )
+      )
 MyCountput.formatted_topn_output(2, ' - Value:', '[Term: ', " ]")
 print(MyCountput.return_as_dict())
