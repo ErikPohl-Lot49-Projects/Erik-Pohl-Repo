8b42d814a08484f1f506827b69eb3194c6ece6ac
Erik Pohl <iotasubscript@gmail.com>
2018-12-15 17:32:46 -0500

    pep8 linted
    pep8 linted

--- python/Countput/Countput_demo_usage.py
+++ python/Countput/Countput_demo_usage.py
@@ -9,20 +9,35 @@ word_sentence = 'a b c d e f g h i a c e g i c g'
 word_list = word_sentence.split(' ')
 
 
-
 MyCountput = Countput(word_list)
-print('return_as_list of strings : ' + str(MyCountput.return_topn_as_list_of_strings(n=2,
-                                                                                     delimiter=' - Value',
-                                                                                     prefix='[Term: ',
-                                                                                     suffix=' ]',
-                                                                                     header='Header' )))
+print('return_as_list of strings : ' +
+      str(
+          MyCountput.return_topn_as_list_of_strings(
+              n=2,
+              delimiter=' - Value',
+              prefix='[Term: ',
+              suffix=' ]',
+              header='Header'
+          )
+      )
+      )
 print('return_as_list of strings for sort_as_list_of_strings: ' +
       str(
           MyCountput.return_topn_as_list_of_strings(n=2)
       )
       )
-MyCountput.formatted_topn_output(n=2, delimiter=' - Value:', prefix='[Term: ', suffix=" ]")
+MyCountput.formatted_topn_output(
+    n=2,
+    delimiter=' - Value:',
+    prefix='[Term: ',
+    suffix=" ]"
+)
 
-MyCountput.formatted_topn_output(n=2, delimiter=' - Value:', prefix='[Term: ', suffix=" ]", header='Formatted Output with header:')
+MyCountput.formatted_topn_output(
+    n=2,
+    delimiter=' - Value:',
+    prefix='[Term: ', suffix=" ]",
+    header='Formatted Output with header:'
+)
 
 print(MyCountput.return_as_dict())
