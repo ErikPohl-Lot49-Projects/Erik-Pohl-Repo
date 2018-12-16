668236d800a06f2b76d1566276281ee0dd8d8b2d
Erik Pohl <iotasubscript@gmail.com>
2018-12-16 16:59:37 -0500

    pep8 linted and use the new input
    pep8 linted and use the new input

--- python/xlate/xlate_demo_usage.py
+++ python/xlate/xlate_demo_usage.py
@@ -11,18 +11,29 @@ from xlate import xlate
 input_string = 'pohl erik 9/2/72 python arlington'
 input_del = ' '
 input_format = ['lname', 'fname', 'bdate', 'language_of_choice', 'hometown']
-demo_usage = xlate(input_string, input_del, input_format)
-
 output_format = '{fname} {lname}, born on {bdate}, ' \
                 'lives in {hometown} and prefers {language_of_choice}'
-print(demo_usage.to_string_using_keyword_format(output_format))
-print(demo_usage.to_dictionary())
+demo_usage = xlate(
+    xlate_input_delimiter=input_del,
+    xlate_input_format=input_format,
+    xlate_output_format=output_format
+)
+
+print(demo_usage.to_string_using_keyword_format(input_string))
+print(demo_usage.to_dictionary(input_string))
 
 output_format = '{0} {1}, born on {2}, lives in {3} and prefers {4}'
-demo_usage = xlate(input_string, input_del)
-print(demo_usage.to_string_using_keyword_format(output_format))
-print(demo_usage.to_dictionary())
+demo_usage = xlate(
+    xlate_input_delimiter=input_del,
+    xlate_output_format=output_format
+)
+print(demo_usage.to_string_using_keyword_format(input_string))
+print(demo_usage.to_dictionary(input_string))
 
 output_format = '{fname} {lname}, born on {bdate}, ' \
                 'lives in {hometown} and prefers {language_of_choice}'
-print(demo_usage.to_string_forcing_positional(output_format))
+demo_usage = xlate(
+    xlate_input_delimiter=input_del,
+    xlate_output_format=output_format
+)
+print(demo_usage.to_string_forcing_positional(input_string))
