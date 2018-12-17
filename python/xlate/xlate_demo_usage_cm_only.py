f014e18eb5e595bf07f23db714671275b34cddf1
Erik Pohl <iotasubscript@gmail.com>
2018-12-16 23:20:31 -0500

    refactor with comprehensions
    refactor with comprehensions

--- python/xlate/xlate_demo_usage_cm_only.py
+++ python/xlate/xlate_demo_usage_cm_only.py
@@ -13,21 +13,23 @@ input_format = ['lname', 'fname', 'bdate', 'language_of_choice', 'hometown']
 output_format = '{fname} {lname}, born on {bdate}, ' \
                 'lives in {hometown} and prefers {language_of_choice}'
 
+input_strings = [
+        'pohl erik 9/2/72 python arlington',
+        'pynchon thomas 5/5/45 muted_posthorn manhattan'
+]
 with xlate_string_formats(
         xlate_input_delimiter=input_del,
         xlate_output_format=output_format
 ) as f:
-    input_string = 'pohl erik 9/2/72 python arlington'
-    print(f(input_string))
-    input_string = 'pynchon thomas 5/5/45 muted_posthorn manhattan'
-    print(f(input_string))
+    [print(f(input_string)) for input_string in input_strings]
 
+input_strings = [
+        'pohl erik 9/2/72 python arlington',
+        'pynchon thomas 5/5/45 muted_posthorn manhattan'
+]
 with xlate_string_formats(
         xlate_input_delimiter=input_del,
         xlate_input_format=input_format,
         xlate_output_format=output_format
 ) as f:
-    input_string = 'pohl erik 9/2/72 python arlington'
-    print(f(input_string))
-    input_string = 'pynchon thomas 5/5/45 muted_posthorn manhattan'
-    print(f(input_string))
+    [print(f(input_string)) for input_string in input_strings]
