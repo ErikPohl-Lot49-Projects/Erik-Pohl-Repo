d7759be6033d0cc1b086f940cdef92d1918fe8a2
Erik Pohl <iotasubscript@gmail.com>
2018-12-18 13:00:24 -0500

    uses a dict as a switch condition
    uses a dict as a switch condition

--- python/switch_class/demo_usage.py
+++ python/switch_class/demo_usage.py
@@ -11,10 +11,12 @@ my_switch.add_switch_clause(['2','3'], 'Three',False)
 my_switch.add_switch_clause(foo(), 'X',False)
 my_switch.add_switch_clause('4', 'Four',True)
 my_switch.add_switch_clause('4', 'Should not get here',True)
+my_switch.add_switch_clause({'6':1}, 'Six',True)
 print(my_switch.execute_switch('1'))
 print(my_switch.execute_switch('2'))
 print(my_switch.execute_switch('5'))
 print(my_switch.execute_switch('4'))
+print(my_switch.execute_switch('6'))
 
 with switch_compare(
     'Not found', 
@@ -23,10 +25,12 @@ with switch_compare(
         (['2','3'], 'Three',False), 
         (foo(), 'X',False), 
         ('4', 'Four',True), 
-        ('4', 'Should not get here',True)
+        ('4', 'Should not get here',True),
+        ({'6':1}, 'Six', True)
         ]
     ) as x:
     print(x('1'))
     print(x('2'))
     print(x('5'))
     print(x('4'))
+    print(x('6'))
