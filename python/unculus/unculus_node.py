762f7e910d9d41dc5e158e2e076df181544e8902
Erik Pohl <iotasubscript@gmail.com>
2018-12-26 13:02:15 -0500

    break on not found
    break on not found

--- python/unculus/unculus_node.py
+++ python/unculus/unculus_node.py
@@ -1,3 +1,19 @@
+from contextlib import contextmanager
+
+''''@contextmanager
+def switch_compare(
+        switch_default, switch_list
+):
+    switch_instance = unculus_node(
+        switch_default
+    )
+    for i in switch_list:
+        switch_instance.add_switch_clause(*i)
+    yield_fun = switch_instance.execute_switch
+    yield yield_fun
+    yield_fun = None
+'''
+
 class unculus_node:
 
     def __init__(self,name):
@@ -11,22 +27,29 @@ class unculus_node:
         for i in self._options:
             if val in i[1]:
                 return i[0]
+        raise ValueError
 
     def consume(self,l):
         print('start', self.name)
         start = self
         for i in l:
-            start = start.eval_val(i)
+            try:
+                start = start.eval_val(i)
+            except:
+                break
             if start is None:
                 break
             print(i, start.name)
 
-    def is_completing_sequence(self, l):
+    def is_consumed_completing_sequence(self, l):
         start = self
         for i in l:
             if start is None:
                 return False
-            start = start.eval_val(i)
+            try:
+                start = start.eval_val(i)
+            except:
+                return False
         return not start
 
 
