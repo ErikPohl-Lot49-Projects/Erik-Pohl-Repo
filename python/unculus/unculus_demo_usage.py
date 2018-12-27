9736c9480fe95265f900eb7411a8e8290430f758
Erik Pohl <iotasubscript@gmail.com>
2018-12-27 17:33:34 -0500

    if you give an unculus a tool
    the unculus will ask for a glass of milk

--- python/unculus/unculus_demo_usage.py
+++ python/unculus/unculus_demo_usage.py
@@ -1,30 +1,43 @@
 from unculus_node import unculus_node
 
-one = unculus_node('one')
-two = unculus_node('two')
 
-one.add_option(None,1)
+def foo(z):
+    print('doing something with value ' + str(z))
+    print(str(z+zappo))
+
+
+def bar(z):
+    print('doing something with entrance token ' + str(z))
+    print(str(z+zappo))
+
+zappo = 2
+one = unculus_node('one', 1, foo, bar)
+two = unculus_node('two', 2, foo, bar)
+
+one.add_option(None, 1)
 one.add_option(two, 2)
-two.add_option(one,3)
-two.add_option(two,4)
+two.add_option(one, 3)
+two.add_option(two, 4)
 
 head = one
-z = [2,3,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,1]
-head.consume(z)
-print(head.is_consumed_completing_sequence(z))
+z = [2, 3, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 1]
+try:
+    head.consume(z)
+except:
+    pass
+try:
+    print(head.is_consumed_completing_sequence(z))
+except:
+    pass
 
-z = [2,3,2,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3]
+z = [2, 3, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3]
 print(head.is_consumed_completing_sequence(z))
 
 z = [1]
 print(head.is_consumed_completing_sequence(z))
 
-z = [2,3,1,2]
+z = [2, 3, 1, 2]
 print(head.is_consumed_completing_sequence(z))
 
-z = [7,3,1,2]
+z = [7, 3, 1, 2]
 print(head.is_consumed_completing_sequence(z))
-
-
-
-
