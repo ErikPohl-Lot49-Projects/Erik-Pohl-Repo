30366fd2a4d09fef48c84026de8ce22212e9032a
Erik Pohl <iotasubscript@gmail.com>
2018-12-28 10:52:00 -0500

    added print all capitals
    added print all capitals

--- python/unculus/bartlett.py
+++ python/unculus/bartlett.py
@@ -1,24 +1,26 @@
 from unculus_node import unculus_node
+from string import ascii_uppercase
+
 
 def collect(z):
     zaz.append(z)
 
+
 def foo():
-    #return ['"','a'] # this will truncate the quote at the 'a'
+    # return ['"','a'] # this will truncate the quote at the 'a'
     return ['"']
 
 bartlett = unculus_node('start')
-bartlett.add_default_turnstile(bartlett,None)
+bartlett.add_default_turnstile(bartlett, None)
 inquotes = unculus_node('inquote')
-bartlett.add_turnstile(['"', '%'],inquotes, None)
+bartlett.add_turnstile(['"', '%'], inquotes, None)
+bartlett.add_turnstile(ascii_uppercase, bartlett, print)
 inquotes.add_default_turnstile(inquotes, collect)
 inquotes.add_turnstile(foo(), bartlett, None)
 
-samplelist = list('Shelby told him, "I would like the sweet tea, please"')
+samplelist = list('Shelby TOLD him, "I would like the sweet tea, please."')
 zaz = []
 start = bartlett
 for n in samplelist:
     start = start.evaluate_token(n)
 print(''.join(zaz))
-
-
