2e60b97ecae46a5d98012c049a140bfc281c611c
Erik Pohl <iotasubscript@gmail.com>
2018-12-28 01:50:52 -0500

    different types of values for the turnstiles
    different types of values for the turnstiles

--- python/unculus/bartlett.py
+++ python/unculus/bartlett.py
@@ -3,12 +3,16 @@ from unculus_node import unculus_node
 def collect(z):
     zaz.append(z)
 
+def foo():
+    #return ['"','a'] # this will truncate the quote at the 'a'
+    return ['"']
+
 bartlett = unculus_node('start')
 bartlett.add_default_turnstile(bartlett,None)
 inquotes = unculus_node('inquote')
-bartlett.add_turnstile('"',inquotes, None)
+bartlett.add_turnstile(['"', '%'],inquotes, None)
 inquotes.add_default_turnstile(inquotes, collect)
-inquotes.add_turnstile('"', bartlett, None)
+inquotes.add_turnstile(foo(), bartlett, None)
 
 samplelist = list('Shelby told him, "I would like the sweet tea, please"')
 zaz = []
