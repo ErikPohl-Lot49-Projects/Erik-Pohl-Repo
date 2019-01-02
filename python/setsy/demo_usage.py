a1c060ea7acafcf706d032d0ab9442c671410fd6
Erik Pohl <iotasubscript@gmail.com>
2019-01-02 10:20:17 -0500

    right
    said fred

--- python/setsy/demo_usage.py
+++ python/setsy/demo_usage.py
@@ -1,7 +1,9 @@
 from setsy import setsy
 
+
 x = setsy([(1, 2), (1, 3)])
 b = setsy([6, 7, 8])
+print('here' , list(b.powerset()))
 print(x.issubset(b))
 print(x.issuperset(b))
 x = x.union(b)  # class setsy which inherits from set returns a set from union
