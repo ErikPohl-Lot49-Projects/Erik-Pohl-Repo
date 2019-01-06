2ae7c235580782686d66ecc125f1ddbebabf22c9
Erik Pohl <iotasubscript@gmail.com>
2019-01-05 21:59:13 -0500

    updated repr
    updated repr

--- python/switch_class/switch_class.py
+++ python/switch_class/switch_class.py
@@ -3,6 +3,7 @@
 
 from contextlib import contextmanager
 
+
 @contextmanager
 def switch_compare(
         switch_default, switch_list
@@ -32,16 +33,16 @@ class switch:
         self.decision_clauses = []
 
     def __repr__(self):
-        z = ''
+        z = 'Switch statement in order of evaluation:\n'
         for i in self.decision_clauses:
-            z=z+"on {switch_condition} return {result} and break on match? {break_on_match}\n".format(**i)
-        z=z+"default value: "+str(self.default_return_value)
-        return z    
-    
+            z = z + "on {switch_condition} return {result} and break on match: ({break_on_match})\n".format(**i)
+        z = z + "default value: " + str(self.default_return_value)
+        return z
+
     def add_switch_clause(self, switch_condition, switch_result, break_on_match):
         '''
         add, in sequence of switching, a switch logic clause with condition,
-        result, and brake instruction 
+        result, and brake instruction
         :param switch_condition: The condition for which the switch result will fire
         :param switch_result: The switch result for a given condition
         :param break_on_match: Halt further switch evaluation on match?
@@ -55,18 +56,18 @@ class switch:
     def execute_switch(self, compare_value):
         '''
         execute_switch
-        send a compare value through the switch to determine any results 
+        send a compare value through the switch to determine any results
         from the switch logic
-        :param compare_value: this is a value to be compared against switch logic 
+        :param compare_value: this is a value to be compared against switch logic
         :return: any results the compare value provoked in the switch logic
         '''
         retval = []
         for decision_clause in self.decision_clauses:
-            switch_condition,\
+            switch_condition, \
             result, \
-            break_on_match = decision_clause['switch_condition'],\
-                                                       decision_clause['result'],\
-                                                       decision_clause['break_on_match']
+            break_on_match = decision_clause['switch_condition'], \
+                             decision_clause['result'], \
+                             decision_clause['break_on_match']
             if compare_value in switch_condition:
                 retval.append(result)
                 if break_on_match:
