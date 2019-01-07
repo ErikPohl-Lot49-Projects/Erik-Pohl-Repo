55f43e0e82b38bafbe98aef3ed44ef6f002e0327
Erik Pohl <iotasubscript@gmail.com>
2019-01-07 09:49:55 -0500

    renamed
    renamed

---
+++ python/numerous_ants/numerous_ants.py
@@ -0,0 +1,41 @@
+import time
+from operator import itemgetter
+from itertools import repeat
+
+class numerous_ants:
+
+    def __init__(self, control_widget, widgets , inputs, iterations):
+        self.control_widget = control_widget
+        self.widgets = widgets
+        self.iterations = iterations
+        self.results = []
+        self.inputs = inputs
+
+    def perform(self,fun, *args):
+        return fun(*args)
+
+    def first_call(self):
+        self.results = []
+        for i in self.widgets:
+            totaltime = 0
+            for k in self.inputs:
+                start = time.time()
+                for _ in repeat(None, self.iterations):
+                    z = self.perform(i[1],k)
+                end = time.time()
+                if z ==self.perform(self.control_widget,k):
+                    totaltime += end-start
+                else:
+                    totaltime = 0
+                    break
+            self.results.append(totaltime)
+
+
+    def resultput(self):
+        l = [(self.widgets[i][0], self.results[i], self.results[i]/sum(self.results)*100) for i,j in enumerate(self.results)]
+        for j in sorted(l, key=itemgetter(1)):
+            print(j)
+
+
+
+
