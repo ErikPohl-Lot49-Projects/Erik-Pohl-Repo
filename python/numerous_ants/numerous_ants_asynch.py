312bb9ca79c357d0e7964fb487c2dd02df870b44
Erik Pohl <iotasubscript@gmail.com>
2019-01-08 21:52:44 -0500

    hang the dj / formicate pep8ed
    hang the dj / formicate pep8ed

--- python/numerous_ants/numerous_ants_asynch.py
+++ python/numerous_ants/numerous_ants_asynch.py
@@ -3,43 +3,67 @@ from operator import itemgetter
 from itertools import repeat
 from multiprocessing import Pool
 
+
 class numerous_ants:
 
-    def __init__(self, control_widget, widgets , inputs, iterations):
+    def __init__(self, control_widget, ants, inputs, iterations):
         self.control_widget = control_widget
-        self.widgets = widgets
+        self.ants = ants
         self.iterations = iterations
         self.results = []
         self.inputs = inputs
 
-    def perform(self,its, fun, *args):
+    def perform(
+            self,
+            iterations,
+            function,
+            *args
+    ):
         start = time.time()
-        for _ in repeat(None, its):
-            z = fun(*args)
+        for _ in repeat(None, iterations):
+            output = function(*args)
         end = time.time()
-        return (z, end-start)
-    
-    def log_result(self,z):
+        return (output, end - start)
+
+    def log_result(self, result_value):
         pass
 
     def formicate(self):
         self.results = []
         pool = Pool()
-        for i in self.widgets:
-            totaltime = 0
-            for k in self.inputs:
-                control_val = self.perform(1, self.control_widget,k)
-                z = pool.apply_async(
-                    self.perform, args = (self.iterations,i[1], k,), callback = self.log_result).get()
-                if z[0] == control_val[0]:
-                    totaltime += z[1]
+        for this_ant in self.ants:
+            total_time = 0
+            for this_input in self.inputs:
+                control_result = self.perform(
+                    1,
+                    self.control_widget,
+                    this_input
+                )
+                experimental_result = pool.apply_async(
+                    self.perform,
+                    args=(
+                        self.iterations,
+                        this_ant[1],
+                        this_input,
+                    ),
+                    callback=self.log_result
+                ).get()
+                if experimental_result[0] == control_result[0]:
+                    total_time += experimental_result[1]
                 else:
-                    totaltime = -1 ## this is a problem
+                    total_time = -1  # this is a problem
                     break
-            self.results.append(totaltime)
-
+            self.results.append(total_time)
 
     def resultput(self):
-        l = [(self.widgets[i][0], self.results[i], self.results[i]/sum(self.results)*100) for i,j in enumerate(self.results)]
-        for j in sorted(l, key=itemgetter(1)):
-            print(j)
+        result_tuples = [
+            (
+                self.ants[result_counter][0],
+                self.results[result_counter],
+                self.results[result_counter] / sum(self.results) * 100
+            )
+            for result_counter, result
+            in enumerate(self.results)
+        ]
+        for result_tuple in sorted(result_tuples, key=itemgetter(1)):
+            print(result_tuple)
