0e6bbdd7c2e48945f7cb2f93b3b6a4737ea162ff
Erik Pohl <iotasubscript@gmail.com>
2019-01-15 15:27:14 -0500

    livid
    "Couldn't remember the Roman numerals for 51, 6 and 500. Livid."

--- python/sneaky_roman_numerals/plural_roman_numerals.py
+++ python/sneaky_roman_numerals/plural_roman_numerals.py
@@ -2,14 +2,21 @@ from itertools import groupby
 
 def plural_rn(roman):
     
-    arabic_digit_places = {
+    base_ten_arabic_digit_places = {
         'M': 0,
         'C': 1,
         'X': 2,
         'I': 3
     }
     
-    subtractive_phrases = ['IV', 'IX', 'XL','XC', 'CD', 'CM']
+    subtractive_phrases_and_factors = [('IV',4), 
+                                       ('IX',9), 
+                                       ('XL',4),
+                                       ('XC',9), 
+                                       ('CD',4), 
+                                       ('CM',9)
+                                       ]
+
     
     five_times_replace_numerals = [
         ('D', 'C'),
@@ -17,28 +24,18 @@ def plural_rn(roman):
         ('L', 'X')
     ]
     
-    subtractive_replace_factor = {
-            'V': 4,
-            'X': 9,
-            'L': 4,
-            'C': 9,
-            'D': 4,
-            'M': 9}
-  
     arabic = ['0','0','0','0']
     
-    for subtractive_phrase in subtractive_phrases:
-        roman = roman.replace(subtractive_phrase, 
-                              subtractive_phrase[0]*subtractive_replace_factor[
-                                  subtractive_phrase[1]
-                                  ]
+    for subtractive_phrase_and_factor in subtractive_phrases_and_factors:
+        roman = roman.replace(subtractive_phrase_and_factor[0], 
+                              subtractive_phrase_and_factor[0][0]*subtractive_phrase_and_factor[1]
                               )
 
     for five_times_replace_numeral in five_times_replace_numerals:
         roman = roman.replace(five_times_replace_numeral[0], 5*five_times_replace_numeral[1])
     
-    process_roman = groupby(roman)
-    for i,j in process_roman:
-        arabic[arabic_digit_places[i]] = str(len(list(j)))
+    roman_numeral_groups = groupby(roman)
+    for index,roman_numeral_group in roman_numeral_groups:
+        arabic[base_ten_arabic_digit_places[index]] = str(len(list(roman_numeral_group)))
     return int(''.join(arabic))
         
