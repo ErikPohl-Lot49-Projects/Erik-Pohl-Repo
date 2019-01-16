a9ca3adc67521f20be4f00779e4bf6b962116ccc
Erik Pohl <iotasubscript@gmail.com>
2019-01-15 21:43:40 -0500

    Too setsy for its car
    Too setsy by far

--- python/setsy/demo_usage.py
+++ python/setsy/demo_usage.py
@@ -12,3 +12,66 @@ print(type(x))
 y = {6, 5, 2, 1}
 print('------------')
 print(x.cartesian(y))
+
+print("is not subset attempt")
+a = setsy([1,2,3])
+print("a:", a)
+b = setsy([2,3,4,5,6,7])
+print("b:", b)
+c = a.is_not_subset(b)
+print("result of a.is_not_subset(b): ", c)
+if c:
+    print("a is not a subset of b")
+
+print("is not superset attempt")
+a = setsy([1,2,3])
+print("a:", a)
+b = setsy([2,3,4,5,6,7])
+print("b:", b)
+c = b.is_not_superset(a)
+print("result of b.is_not_superset(a): ", c)
+if c:
+    print("b is not a superset of a")
+
+print("is not subset attempt")
+a = setsy([2,3])
+print("a:", a)
+b = setsy([2,3,4,5,6,7])
+print("b:", b)
+c = a.is_not_subset(b)
+print("result of a.is_not_subset(b): ", c)
+print("a is not a subset of b") if c else print("a is a subset of b")
+
+print("is not superset attempt")
+a = setsy([2,3])
+print("a:", a)
+b = setsy([2,3,4,5,6,7])
+print("b:", b)
+c = b.is_not_superset(a)
+print("result of b.is_not_superset(a): ", c)
+if c:
+    print("b is not a superset of a")
+
+print("is not subset attempt")
+a = setsy([2,3])
+print("a:", a)
+b = setsy([2])
+print("b:", b)
+c = a.is_not_subset(b)
+print("result of a.is_not_subset(b): ", c)
+print("a is not a subset of b") if c else print("a is a subset of b")
+
+print("is not superset attempt")
+a = setsy([2,3])
+print("a:", a)
+b = setsy([2])
+print("b:", b)
+c = b.is_not_superset(a)
+print("result of b.is_not_superset(a): ", c)
+if c:
+    print("b is not a superset of a")
+
+
+
+
+
