64dcf3539352802c14ef0bf882e479b05d47d6c9
Erik Pohl <iotasubscript@gmail.com>
2019-01-17 17:12:31 -0500

    test script beginning
    test script beginning

---
+++ python/switch_class/test_switch_class.py
@@ -0,0 +1,7 @@
+import pytest
+from switch_class import switch_class
+
+def test_initialize():
+    default = 'not found'
+    myswitch = switch_class.switch(default)
+    assert myswitch.default_return_value == default 
