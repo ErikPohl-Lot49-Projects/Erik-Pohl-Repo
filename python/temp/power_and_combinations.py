f1ec696bc2960e4410c9f6ded458c793f4d8a0aa
Erik Pohl <iotasubscript@gmail.com>
2019-01-23 14:07:17 -0500

    added a better way to do the power set
    added a better way to do the power set

--- python/temp/power_and_combinations.py
+++ python/temp/power_and_combinations.py
@@ -15,6 +15,18 @@ def combinations(items, combination_length):
                 max_item_position = [items[item_position]]
                 max_item_position.extend(sub_combination)
                 yield max_item_position
+
+def power2(items):
+    power_count = 2**len(items)
+    for power_mask in range(power_count):
+        power_mask_string = str(bin(power_mask))[2:].zfill(len(items))
+        combination = []
+        for position,onoff in enumerate(power_mask_string):
+            if onoff=='1':
+                combination.append(items[position])
+        print (combination)
+    
+    
             
 def power(items):
     for combination_length in range(len(items)+1):
@@ -22,5 +34,7 @@ def power(items):
             print(single_length_combination)
             
 demo_list = [1,2,3,4,5,6,7]
+
 power(demo_list)
 power([])
+power2(demo_list)
