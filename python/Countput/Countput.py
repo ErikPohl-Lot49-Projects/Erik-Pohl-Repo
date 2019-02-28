b117e617eff80c1879630560291d126bfda1cc92
Erik Pohl <iotasubscript@gmail.com>
2019-02-28 09:41:11 -0500

    use docsys
    use docysys for comments

--- python/Countput/Countput.py
+++ python/Countput/Countput.py
@@ -12,7 +12,8 @@ import re
 # from ..version_compare import version_compare
 
 class version_compare:
-    '''Compare the current compiling version Python against a benchmark to determine which code to use''' 
+    '''Compare the current compiling version Python against a benchmark to determine which code to use'''
+     
     def __init__(self):
         self._current_version = sys.version.split(' ')[0]
 
@@ -38,6 +39,11 @@ class Countput(Counter):
             suffix='',
             header=None
     ):
+        '''
+        Return the top n of the counter object
+        as a list of strings
+        using parameter hints for formatting
+        '''
         headless_horseman = [
             prefix + delimiter.join(
                 [
@@ -62,6 +68,11 @@ class Countput(Counter):
             suffix='',
             header=None
     ):
+        '''
+        Output to stdout the top n 
+        of the counter object
+        using parameter hints for formatting
+        '''
         if header:
             print(header)
         [
@@ -81,9 +92,15 @@ class Countput(Counter):
         ]
 
     def return_as_dict(self,frequency = None):
+        '''
+        Return the Counter results
+        as a dictionary 
+        using an optional frequency parameter
+        to limit results
+        '''
+        
         # do something different for versions of Python
         # where the dictionary is not automatically ordered
-
         VC = version_compare();
         # adding to an ordered dict takes 75% more time
         return_dictionary = {} if VC.current_version_greater_than_or_equal_than("3.7.1") else OrderedDict()
